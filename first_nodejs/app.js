a245fd5e099a28f1e226d192830dd18e3fc15a6c
koyamal <tanakamaruko1121n@gmail.com>
2021-12-13 21:48:08 +0900

    Add second page

--- first_nodejs/app.js
+++ first_nodejs/app.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
 const mysql = require('mysql');
 
 const app = express();
 
 app.get('/', (req, res) => {
   res.render('index.ejs');
+});
+
+app.get('/second/:id', (req, res) =>{
+  res.render('second.ejs', {itemId: req.params.id});
 });
 
 app.listen(3000);
