7d9baf4c5d6cb5afe514e4b06faaec460bbd5b7a
koyamal <tanakamaruko1121n@gmail.com>
2021-12-14 22:26:06 +0900

    Add DB (MySQL)

--- first_nodejs/app.js
+++ first_nodejs/app.js
@@ -5,12 +5,32 @@ const mysql = require('mysql');
 
 const app = express();
 
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'password',
+  database: 'first'
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.log('error connecting: ' + err.stack);
+    return;
+  }
+  console.log('success');
+});
+
 app.get('/', (req, res) => {
   res.render('index.ejs');
 });
 
 app.get('/second/:id', (req, res) =>{
-  res.render('second.ejs', {itemId: req.params.id});
+  connection.query(
+    'SELECT * FROM users',
+    (error, results) =>{
+      res.render('second.ejs', {itemId: req.params.id, userInfo: results[0]});
+    }
+  );
 });
 
 app.listen(3000);
