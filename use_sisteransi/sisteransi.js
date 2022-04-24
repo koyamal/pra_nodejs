f6facb4f9b15d514f1fceb2da57093a13f6b1d06
koyamal <tanakamaruko1121n@gmail.com>
2022-04-24 17:08:09 +0900

    use api of sisteransi

--- use_sisteransi/sisteransi.js
+++ use_sisteransi/sisteransi.js
@@ -15,3 +15,21 @@ const p = str => process.stdout.write(str);
 
 //p(ansi.cursor.forward(12) + ansi.cursor.backward(11));
 
+//p(ansi.cursor.nextLine(12));
+
+//p(ansi.cursor.prevLine(3));
+
+//p(ansi.cursor.hide);
+
+//p(ansi.cursor.show);
+
+//p(ansi.cursor.left);
+
+//p(ansi.scroll.up(200));
+//p(ansi.scroll.down(10));
+
+//p(ansi.erase.screen);
+//p(ansi.erase.up(4));
+
+//p(ansi.erase.line);
+p(ansi.erase.lines(4));
