dd411f1c97be3e327244f721603f83be21293f84
koyamal <tanakamaruko1121n@gmail.com>
2022-04-24 14:38:31 +0900

    use Chained Methods

--- use_kleur/kleur.js
+++ use_kleur/kleur.js
@@ -1,2 +1,16 @@
 const kleur = require('kleur');
 console.log(kleur.red('red text'));
+console.log(kleur.green('green text'));
+
+//const bold = require('kleur');
+
+//console.log(bold.red('this is a bold red message'));
+//console.log(bold.italic('this is a bold italicized message'));
+//console.log(bold.yellow().bgRed().italic('this is a bold yellow italicized message'));
+
+const { bold, green } = require('kleur');
+
+console.log(bold().red('this is a bold red message'));
+console.log(bold().italic('this is a bold italicized message'));
+console.log(bold().yellow().bgRed().italic('this is a bold yellow italicized message'));
+console.log(green().bold().underline('this is a bold green underlined message'));
