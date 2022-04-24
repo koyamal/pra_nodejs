6f76eaa2fbcb168b437019393a57b8bd8780c403
koyamal <tanakamaruko1121n@gmail.com>
2022-04-24 14:48:21 +0900

    use modifiers of kleur

--- use_kleur/kleur.js
+++ use_kleur/kleur.js
@@ -14,3 +14,17 @@ console.log(bold().red('this is a bold red message'));
 console.log(bold().italic('this is a bold italicized message'));
 console.log(bold().yellow().bgRed().italic('this is a bold yellow italicized message'));
 console.log(green().bold().underline('this is a bold green underlined message'));
+
+const { yellow, red, cyan } = require('kleur');
+
+console.log(yellow(`foo ${red().bold('red')} bar ${cyan('cyan')} baz`));
+console.log(yellow('foo ' + red().bold('red') + ' bar ' + cyan('cyan') + ' baz'));
+
+console.log(kleur.reset('reset'));
+console.log(kleur.bold('bold'));
+console.log(kleur.dim('dim'));
+console.log(kleur.italic('italic'));
+console.log(kleur.underline('underline'));
+console.log(kleur.inverse('inverse'));
+console.log(kleur.hidden('hidden'));
+console.log(kleur.strikethrough('strikethrough'));
