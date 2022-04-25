b5bf804adcc9f349c0929ca6123539ccd62cc46e
koyamal <tanakamaruko1121n@gmail.com>
2022-04-25 21:25:58 +0900

    check flow of code

--- test_require/elements/fns.js
+++ test_require/elements/fns.js
@@ -1,9 +1,12 @@
 const $ = exports;
 
 function test(type, args){
-    return console.log(type, args);
+    console.log(type, args);
 }
 
 $.text = args => test('text', args);
 
-console.log($.text('hello'));
+//console.log('test message01');
+//$.text('test message02');
+//console.log($.text('test message02')); text test message02, undefined
+//console.log('test message03');
