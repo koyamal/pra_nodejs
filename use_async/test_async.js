8dacdcbe341765b8838e42adb423759e7572a02e
koyamal <tanakamaruko1121n@gmail.com>
2022-04-26 22:45:18 +0900

    use setTimeout

--- use_async/test_async.js
+++ use_async/test_async.js
@@ -1,13 +1,18 @@
 function myPromise(num) {
     return new Promise(function(resolve) {
         setTimeout(function() { resolve(num * num) }, 3000)
-    })
+    });
+}
+
+function testasync(text){
+    return setTimeout(function(){ console.log(text)}, 3000);
 }
 
 async function myAsync() {
     console.log('start');
     const result = await myPromise(10);
     console.log(result);
+    const output = await testasync('Hello');
     console.log('end');
 }
  
