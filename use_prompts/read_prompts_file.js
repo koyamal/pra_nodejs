1070140fc9d80aabab9a3003e944f708eb334747
koyamal <tanakamaruko1121n@gmail.com>
2022-05-11 20:22:02 +0900

    Check output with none value

--- use_prompts/read_prompts_file.js
+++ use_prompts/read_prompts_file.js
@@ -8,7 +8,7 @@ let questions = [
     {
         type: 'number',
         name: 'value02',
-        message: 'How old are you?',
+        //message: 'How old are you?',
         validate: value => value < 18 ? `Nightclub is 18+ only` : true
     },
 ];
@@ -18,8 +18,9 @@ questions = [].concat(questions);
 console.log(questions);
 
 for (question of questions){
-    ({ name, type } = question);
+    ({ name, type, message} = question);
     console.log(name);
     console.log(type);
+    console.log(message);
     console.log(question);
 }
