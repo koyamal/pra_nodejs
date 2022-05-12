6286a80221e6a22b8841f3dac13cc0ab4f0f183c
koyamal <tanakamaruko1121n@gmail.com>
2022-05-12 21:07:36 +0900

    Add g function in test async2

--- use_async/test_async2.js
+++ use_async/test_async2.js
@@ -1,15 +1,28 @@
 async function f() {
     let promise = new Promise((resolve, reject) => {
-      setTimeout(() => resolve("done!"), 1500)
+      setTimeout(() => resolve("f: done!"), 1500)
       //setTimeout(() => resolve(console.log("hello")), 1000)
     });
     console.log('2');
     console.log(promise);
     let result = await promise;
-    console.log("4");
+    console.log("6");
     console.log(result, promise);
+}
+
+async function g() {
+  let promise = new Promise((resolve, reject) => {
+    setTimeout(() => resolve("g: done!"), 1000)
+    //setTimeout(() => resolve(console.log("hello")), 1000)
+  });
+  console.log('4');
+  console.log(promise);
+  let result = await promise;
+  console.log("5");
+  console.log(result, promise);
 }
 
 console.log('1');
 f();
 console.log('3');
+g();
