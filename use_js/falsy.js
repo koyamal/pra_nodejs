3cf2938f9f960b78c2d16074c69a09f436475a7f
koyamal <tanakamaruko1121n@gmail.com>
2022-05-15 15:57:12 +0900

    Check || behavior

--- use_js/falsy.js
+++ use_js/falsy.js
@@ -3,3 +3,13 @@ let val;
 const uName = val || 'Tom';
 
 console.log(uName);
+
+let a = 1;
+let b = 0;
+let c = 3;
+
+console.log(a || b); // 1(a)
+console.log(b || a); // 1(a)
+console.log(a || c); // 1(a)
+console.log(c || a); // 3(c)
+console.log(a || b || c); //1(a)
