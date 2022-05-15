2eefe13efadb1b5e1fde78ce7db6118c2d6ac591
koyamal <tanakamaruko1121n@gmail.com>
2022-05-15 15:55:18 +0900

    Check && behavior

--- use_js/truthy.js
+++ use_js/truthy.js
@@ -1,4 +1,11 @@
 let a = 1;
 let b = 0;
+let c = 3;
 a && console.log('Hello');
 b && console.log('Bye');
+
+console.log(a && b); // 0(b)
+console.log(b && a); // 0(b)
+console.log(a && c); // 3(c)
+console.log(c && a); // 1(a)
+console.log(a && b && c); //0(b)
