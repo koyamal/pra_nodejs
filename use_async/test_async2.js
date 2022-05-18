8a8d911a8b31303cde309bb8e9b1de8f0b934ba6
koyamal <tanakamaruko1121n@gmail.com>
2022-05-18 21:43:12 +0900

    Delete Unnecessary Lines

--- use_async/test_async2.js
+++ use_async/test_async2.js
@@ -1,24 +1,20 @@
 async function f() {
     let promise = new Promise((resolve, reject) => {
       setTimeout(() => resolve("6(f: done!)"), 1500)
-      //setTimeout(() => resolve(console.log("hello")), 1000)
     });
     console.log('2');
     console.log(promise);
     let result = await promise;
-    //console.log("6");
     console.log(result);
 }
 
 async function g() {
   let promise = new Promise((resolve, reject) => {
     setTimeout(() => resolve("5(g: done!)"), 1400)
-    //setTimeout(() => resolve(console.log("hello")), 1000)
   });
   console.log('4');
   console.log(promise);
   let result = await promise;
-  //console.log("5");
   console.log(result);
 }
 
