d663a9b693d2b0dbc9f849cc5e9a690c5ca8cbf1
koyamal <tanakamaruko1121n@gmail.com>
2022-05-19 21:08:38 +0900

    Check change const value

--- use_js/object.js
+++ use_js/object.js
@@ -8,3 +8,12 @@ console.log('a.prop: ' + a.prop);
 b.prop = 'Bye';
 
 console.log('a.prop: ' + a.prop);
+
+
+const c = {
+    prop: 'Hello'
+};
+console.log('c.prop: ' + c.prop);
+c.prop = 'Bye';
+console.log('c.prop: ' + c.prop);
+//c = {} //エラー
