5bc715af3fddb2aab69f9b7ad32a0ac62657e3f6
koyamal <tanakamaruko1121n@gmail.com>
2022-05-19 21:45:31 +0900

    Check equality of object

---
+++ use_js/equality_object.js
@@ -0,0 +1,11 @@
+let a = {
+    prop: 0
+};
+
+let b = {
+    prop: 0
+}
+
+console.log('a === b: ', a === b);
+console.log('a == b: ', a == b);
+console.log('a.prop === b.prop: ', a.prop === b.prop);
