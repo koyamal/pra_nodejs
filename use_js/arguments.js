cbae645cf97ece3e7309bbdb40ad2c696371bd7a
koyamal <tanakamaruko1121n@gmail.com>
2022-05-20 18:22:15 +0900

    Check arguments at function

---
+++ use_js/arguments.js
@@ -0,0 +1,15 @@
+function fn(a, b=1){
+    console.log('fn');
+    console.log(arguments);
+    console.log(a, b);
+}
+
+fn(1);
+fn(1, 2, 3);
+
+function fn2(){
+    console.log('fn2');
+    console.log(arguments);
+}
+
+fn2(12, 43, 45);
