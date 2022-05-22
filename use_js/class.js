b2dda64e07eaecfa3d070690811df6aee1aa77f9
koyamal <tanakamaruko1121n@gmail.com>
2022-05-22 12:04:48 +0900

    Check class and inheritance class

---
+++ use_js/class.js
@@ -0,0 +1,29 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    hello(){
+        console.log('hello ' + this.name);
+    }
+}
+
+class Japanese extends Person{
+    constructor(name, age, gender){
+        super(name, age);
+        this.gender = gender;
+    }
+
+    hello(){
+        console.log('kon ' + this.name);
+    }
+
+    bye(){
+        console.log('Sayo ' + this.name);
+    }
+}
+
+const bob = new Japanese('Bob', 34, 'Male');
+console.log(bob);
+bob.bye();
