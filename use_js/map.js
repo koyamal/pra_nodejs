18b3883b0d09ecd3bcf8ca1c7ed11f0fc7bbd97d
koyamal <tanakamaruko1121n@gmail.com>
2022-05-26 21:24:03 +0900

    Edit map . js

--- use_js/map.js
+++ use_js/map.js
@@ -1,8 +1,12 @@
 const map = new Map();
 const key1 = {};
+const key10 = {};
 map.set(key1, 'value1');
 console.log(map.get(key1)); //value1
 console.log(map.get({})); //undefined
+
+map.set(key10, 'value10');
+console.log(map.get(key1)); //value1
 
 const key2 = function(){}
 map.set(key2, 'value2');
@@ -13,6 +17,54 @@ map.set(key3, 'value3');
 console.log(map.get(key3)); //value3
 console.log(map.get(0)); //value3
 
+const key4 = 'keyword';
+map.set(key4, 'value4');
+const key5 = 'keyword';
+map.set(key5, 'value5');
+
 for(const m of map){
     console.log(m);
 }
+
+
+
+const events = (function() {
+	const eventStack = new Map();
+
+	return {
+		on(type, fn) {
+			const fnStack = eventStack.get(type) || new Set();
+			fnStack.add(fn);
+			eventStack.set(type, fnStack);
+		},
+		off(type, fn) {
+			const fnStack = eventStack.get(type);
+			if(fnStack && fnStack.has(fn)) {
+				fnStack.delete(fn);
+			}
+		},
+		emit(type, _this) {
+			const fnStack = eventStack.get(type);
+			if(fnStack) {
+				for(const fn of fnStack) {
+					fn.call(_this);
+				}
+			}
+		}
+	}
+})();
+
+events.on('test', function(){console.log('test1')});
+events.on('test', function(){console.log('test2')});
+events.emit('test', this);
+
+
+const setmap = function(map, key, val){
+    return map.set(key, val);
+}
+
+const newMap = new Map();
+setmap(newMap, 'tests', 'test01');
+console.log(newMap);
+setmap(newMap, 'tests', 'test02');
+console.log(newMap);
