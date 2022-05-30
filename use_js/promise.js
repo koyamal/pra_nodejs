6e273b6fced8c23ee24e1dee5a3dc7bcce426ca2
koyamal <tanakamaruko1121n@gmail.com>
2022-05-30 21:57:38 +0900

    Check promise then catch finally behavior

---
+++ use_js/promise.js
@@ -0,0 +1,18 @@
+new Promise(function(resolve, reject){
+    console.log('promise');
+    //resolve('hello');
+    reject('bye');
+}).then(function(data){
+    console.log('then1: ' + data);
+    //throw new Error();
+    return data + 'o';
+}).then(function(data){
+    console.log('then2: ' + data);
+    return data + 'o';
+}).catch(function(data){
+    console.log('catch: ' + data);
+}).finally(function(data){
+    console.log('finally: ' + data); //finally undefined
+})
+
+console.log('global end');
