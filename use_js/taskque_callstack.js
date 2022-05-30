5babd598bc21cd94c9dfcf95e9dcdd73a30118f3
koyamal <tanakamaruko1121n@gmail.com>
2022-05-30 22:18:07 +0900

    Check first in first out behavior of task que

--- use_js/taskque_callstack.js
+++ use_js/taskque_callstack.js
@@ -3,6 +3,14 @@ function a(){
         console.log('task1 done');
     }, 4000);
 
+    setTimeout(function task2(){
+        console.log('task2 done');
+    }, 3000);
+
+    setTimeout(function task3(){
+        console.log('task3 done');
+    }, 3500);
+
     const startTime = new Date();
     while(new Date() - startTime < 6000);
 
