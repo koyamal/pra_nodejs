593b1c612031fed0f6b6b8c6151c95a06609c7ac
koyamal <tanakamaruko1121n@gmail.com>
2022-06-03 21:45:23 +0900

    Add promise all to promise chain

--- use_js/promise_all.js
+++ use_js/promise_all.js
@@ -11,3 +11,14 @@ Promise.all([sleep(2), sleep(3), sleep(4)]).then(function(data){
     console.log('end');
     console.log(data);
 });
+
+
+sleep(0).then(function(val){
+    return sleep(val);
+}).then(function(val){
+    return Promise.all([sleep(2), sleep(3), sleep(4)]);
+}).then(function(val){
+    return sleep(val);
+}).then(function(val){
+    return sleep(val);
+})
