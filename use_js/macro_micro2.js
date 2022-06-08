e2bc6e3c5b170bd17beaee32f0e914055a357e25
koyamal <tanakamaruko1121n@gmail.com>
2022-06-08 21:39:12 +0900

    Check order macro and micro

--- use_js/macro_micro2.js
+++ use_js/macro_micro2.js
@@ -7,6 +7,13 @@ new Promise(function promise(resolve){
     });
 }).then(function job1(){
     console.log('job1');
+    setTimeout(function task2(){
+        console.log('task2');
+    });
+}).then(function job2(){
+    console.log('job2');
+}).then(function job3(){
+    console.log('job3');
 });
 
 console.log('global end');
