99491d9cf2ab962dd0a28377338306ee03230c4e
koyamal <tanakamaruko1121n@gmail.com>
2022-06-16 21:15:55 +0900

    Try post body but error

--- use_js/fetch2/main.js
+++ use_js/fetch2/main.js
@@ -1,5 +1,5 @@
 async function fetchLocal(){
-    const response = await fetch('http://localhost:3000/add_fruit_temp', {method: 'POST'});
+    const response = await fetch('http://localhost:3000/add_fruit_temp', {method: 'POST', body: {a: 'from_another_origin'}});
     console.log(response);
 }
 
