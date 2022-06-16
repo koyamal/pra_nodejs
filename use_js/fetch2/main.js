25084caa1ef1423057d49c37e703c0b42c2eb801
koyamal <tanakamaruko1121n@gmail.com>
2022-06-16 20:57:18 +0900

    Change URL and Method of fetch

--- use_js/fetch2/main.js
+++ use_js/fetch2/main.js
@@ -1,5 +1,5 @@
 async function fetchLocal(){
-    const response = await fetch('http://localhost:3000', {method: 'GET'});
+    const response = await fetch('http://localhost:3000/add_fruit_temp', {method: 'POST'});
     console.log(response);
 }
 
