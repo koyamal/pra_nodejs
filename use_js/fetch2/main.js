0a84db01da58394269c47c6015743dbc0d722cf4
koyamal <tanakamaruko1121n@gmail.com>
2022-06-17 18:47:39 +0900

    Edit for push body but false

--- use_js/fetch2/main.js
+++ use_js/fetch2/main.js
@@ -1,5 +1,12 @@
 async function fetchLocal(){
-    const response = await fetch('http://localhost:3000/add_fruit_temp', {method: 'POST', body: {a: 'from_another_origin'}});
+    const response = await fetch('http://localhost:3000/add_fruit_temp', {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({a: 'from_another_origin'})
+    });
     console.log(response);
 }
 
