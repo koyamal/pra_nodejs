67ea246b6cb10a02e2dd5a3af8a3b94e933ac6e1
koyamal <tanakamaruko1121n@gmail.com>
2022-06-21 22:00:50 +0900

    Solve error for fetch

--- use_js/fetch2/main.js
+++ use_js/fetch2/main.js
@@ -1,13 +1,23 @@
+// async function fetchLocal(){
+//     const response = await fetch('http://localhost:3000/add_fruit_temp', {
+//         method: 'POST',
+//         headers: {
+//             Accept: 'application/json',
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify({a: 'from_another_origin'})
+//     });
+//     console.log(response);
+// }
 async function fetchLocal(){
     const response = await fetch('http://localhost:3000/add_fruit_temp', {
         method: 'POST',
-        headers: {
-            Accept: 'application/json',
-            'Content-Type': 'application/json',
-        },
+        // headers: {
+        //     Accept: 'application/json',
+        //     'Content-Type': 'application/json',
+        // },
         body: JSON.stringify({a: 'from_another_origin'})
     });
     console.log(response);
 }
-
 fetchLocal();
