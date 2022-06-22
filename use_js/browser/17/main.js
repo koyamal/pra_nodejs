5cfea31a45d375e24fc355575695b558709defea
koyamal <tanakamaruko1121n@gmail.com>
2022-06-22 19:22:00 +0900

    Check hoisting

---
+++ use_js/browser/17/main.js
@@ -0,0 +1,20 @@
+a();
+function a(){
+    console.log('a is called');
+}
+
+console.log(b);
+var b = 0;
+console.log(b);
+// var b;
+// console.log(b);
+// b = 0;
+
+d();
+const d = function(){
+    console.log('d is called');
+}
+
+console.log(c);
+//let c = 0;
+const c = 0;
