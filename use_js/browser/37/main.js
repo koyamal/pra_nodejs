de502a71288614f7ac04127194d511ae01d98527
koyamal <tanakamaruko1121n@gmail.com>
2022-06-28 08:24:47 +0900

    Check difference of let, const and var

---
+++ use_js/browser/37/main.js
@@ -0,0 +1,21 @@
+let a = 0;
+//let a = 0;
+// error
+
+var b = 0;
+var b = 1;
+console.log(b);
+
+{
+    let c = 0;
+    var d = 1;
+}
+
+// console.log(c);
+// error
+console.log(d);
+// console.log(e);
+// error
+console.log(f);
+let e = 0;
+var f = 1;
