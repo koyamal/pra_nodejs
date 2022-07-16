71900d9ea21cccbc999e7d6a4b3ed96e74bcf538
koyamal <tanakamaruko1121n@gmail.com>
2022-07-16 09:46:57 +0900

    Check constructor function

---
+++ use_js/browser/78/main.js
@@ -0,0 +1,8 @@
+function Person(name, age){
+    this.name = name;
+    this.age = age;
+}
+
+const bob = new Person('Bob', 19);
+const tom = new Person('Tom', 32);
+const sun = new Person('Sun', 32);
