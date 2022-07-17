e6d7b203743c1bbe0459142b208ba96887badf0d
koyamal <tanakamaruko1121n@gmail.com>
2022-07-17 17:48:27 +0900

    Edit function constructor

--- use_js/browser/82/main.js
+++ use_js/browser/82/main.js
@@ -2,10 +2,14 @@ const fn1 = new Function('a', 'b', 'return a + b');
 
 const result = fn1(1, 2);
 
+console.log(fn1 instanceof Function);
 console.log(result);
 
 function fn2(a, b){
     return a + b;
 }
 
+const result2 = fn2(1, 3);
+
 console.log(fn2 instanceof Function);
+console.log(result2);
