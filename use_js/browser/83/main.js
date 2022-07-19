8667025c1e17a7865cee130cf6f4003c5a4de670
koyamal <tanakamaruko1121n@gmail.com>
2022-07-19 21:36:04 +0900

    Check prototype chain

--- use_js/browser/83/main.js
+++ use_js/browser/83/main.js
@@ -1,11 +1,21 @@
 function Person(name, age){
     this.name = name;
     this.age = age;
+    this.hello = function(){
+        console.log('OwnProperty: hello ' + this.name); //first order
+    }
 }
 
 Person.prototype.hello = function(){
-    console.log('hello ' + this.name);
+    console.log('Person: hello ' + this.name); //second order
+}
+
+Object.prototype.hello = function(){
+    console.log('Object: hello ' + this.name); //third order
 }
 
 const bob = new Person('bob', 18);
 bob.hello();
+
+const result = bob.hasOwnProperty('name');
+console.log(result);
