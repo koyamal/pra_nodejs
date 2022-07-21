cb5394618f1b0540305f283840d2d42ceb19c97e
koyamal <tanakamaruko1121n@gmail.com>
2022-07-21 21:37:17 +0900

    Check Wrapper Object

---
+++ use_js/browser/90/main.js
@@ -0,0 +1,9 @@
+const a = new String('hello');
+console.log(a);
+console.log(a.toUpperCase());
+
+const b = new Number(100);
+console.log(b.toExponential());
+
+const c = 'hello'.toUpperCase();
+console.log(c);
