38c5d4d40d8d9a8a6cde580601eae85279771c8b
koyamal <tanakamaruko1121n@gmail.com>
2023-02-25 17:19:12 +0900

    back to original code

--- use_js/browser/2023/21/main.js
+++ use_js/browser/2023/21/main.js
@@ -1,14 +1,13 @@
-{
-    let a = 0;
-    console.log(a);
+let a = 0;
+var b = 0;
+function c() {
+
 }
-
-// error
-// console.log(a); 
-
-{
-    var b = 0;
-    console.log(b);
-}
-
+console.log(window.b);
 console.log(b);
+
+window.d = 1;
+let d = 2;
+console.log(window.d);
+console.log(d);
+debugger;
