7c7495a6df3109af89c994bf1b93dd955224477e
koyamal <tanakamaruko1121n@gmail.com>
2023-02-25 16:53:18 +0900

    learn 17

---
+++ use_js/browser/2023/17/main.js
@@ -0,0 +1,20 @@
+a();
+
+function a() {
+    console.log('a is called');
+}
+
+//a();
+
+console.log(b); // undefined
+var b = 0;
+
+// var b;
+// console.log(b);
+// b = 0;
+
+console.log(c);
+let c = 0;
+
+console.log(d);
+let d = 0;
