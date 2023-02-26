0af6c6d9e3a289b88349952be905d7d6b0dbd61f
koyamal <tanakamaruko1121n@gmail.com>
2023-02-26 17:33:17 +0900

    learn 49

---
+++ use_js/browser/2023/49/main.js
@@ -0,0 +1,14 @@
+const a = {
+    prop: 1
+}
+
+const b = {
+    prop: 1
+}
+
+console.log('a === b: ', a === b);
+console.log('a.prop === b.prop: ', a.prop === b.prop);
+
+const c = a;
+
+console.log('a === c: ', a === c);
