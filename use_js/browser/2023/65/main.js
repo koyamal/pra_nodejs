0158e02b6e64be880313c08b76b75a7799c661b1
koyamal <tanakamaruko1121n@gmail.com>
2023-02-28 20:53:42 +0900

    learn 65

---
+++ use_js/browser/2023/65/main.js
@@ -0,0 +1,17 @@
+function a(name){
+    return 'Hello ' + name;
+}
+
+const b = function(name){
+    return 'Hello ' + name;
+}
+
+const c = (name) => {
+    return 'Hello ' + name;
+}
+
+const d = name => {
+    return 'Hello ' + name;
+}
+
+const e = name => 'Hello ' + name;
