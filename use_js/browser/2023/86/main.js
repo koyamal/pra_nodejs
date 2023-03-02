1a9cdc8a1b4d32baa9f8ca3f236b32ad03ccfbeb
koyamal <tanakamaruko1121n@gmail.com>
2023-03-02 20:53:16 +0900

    learn 86

--- use_js/browser/2023/86/main.js
+++ use_js/browser/2023/86/main.js
@@ -1,8 +1,22 @@
-function Person(name, age){
-    this.name = name;
-    this.age = age;
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    hello(){
+        console.log('Person: hello ' + this.name);
+    }
 }
 
-Person.prototype.hello = function(){
-    console.log('Person: hello ' + this.name);
-}
+// function Person(name, age){
+//     this.name = name;
+//     this.age = age;
+// }
+
+// Person.prototype.hello = function(){
+//     console.log('Person: hello ' + this.name);
+// }
+
+const bob = new Person('bob', 32);
+console.log(bob);
