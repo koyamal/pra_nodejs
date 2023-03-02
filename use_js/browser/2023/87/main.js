951796f48c7aaf91b5b4042934fe76ee5c4d9071
koyamal <tanakamaruko1121n@gmail.com>
2023-03-02 20:58:20 +0900

    learn 87

---
+++ use_js/browser/2023/87/main.js
@@ -0,0 +1,26 @@
+class Person{
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    hello(){
+        console.log('Person: hello ' + this.name);
+    }
+}
+
+class Japanese extends Person{
+    constructor(name, age, gender){
+        super(name, age);
+        this.gender = gender
+    }
+
+    bye(){
+        console.log('Japanese: さようなら ' + this.name);
+    }
+}
+
+const taro = new Japanese('taro', 23, 'Male');
+console.log(taro);
+taro.hello();
+taro.bye();
