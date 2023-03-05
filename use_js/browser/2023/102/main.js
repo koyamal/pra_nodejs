e4207e0bab6b70575301f5b0efd6af36c211ab5c
koyamal <tanakamaruko1121n@gmail.com>
2023-03-05 10:12:05 +0900

    learn 102

---
+++ use_js/browser/2023/102/main.js
@@ -0,0 +1,13 @@
+for(let i = 0; i < 10; /*i = i + 1*/ i++){
+    console.log(i);
+}
+
+while(false){
+    console.log('hello');
+}
+
+let i = 0;
+while(i < 10){
+    console.log('hello');
+    i++;
+}
