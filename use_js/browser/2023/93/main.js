26ab6fae9894fdc1d4b0c1e095790b6df062ccc5
koyamal <tanakamaruko1121n@gmail.com>
2023-03-05 09:48:15 +0900

    learn 93

--- use_js/browser/2023/93/main.js
+++ use_js/browser/2023/93/main.js
@@ -9,11 +9,29 @@ Object.defineProperty(Person1.prototype, 'name', {
         return this._name;
     },
     set: function(val){
-        this._name = 'Taro';
-        // this._name = val;
+        // this._name = 'Taro';
+        this._name = val;
     }
 });
 
 const p1 = new Person1('Bob', 23);
 p1.name = 'Tom';
 console.log(p1.name);
+
+class Person2{
+    constructor(name, age)
+    {
+        this._name = name;
+        this._age = age;
+    }
+
+    get name(){
+        // return 'hello';
+        return this._name;
+    }
+
+    set name(val){
+        // this._name = 'Taro';
+        this._name = val;
+    }
+}
