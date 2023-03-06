8c0900b1e071688cb4010fa848e3de097f46af4d
koyamal <tanakamaruko1121n@gmail.com>
2023-03-07 08:38:16 +0900

    learn 122

---
+++ use_js/browser/2023/122/main.js
@@ -0,0 +1,16 @@
+function sleep(ms){
+    const startTime = new Date();
+    while(new Date() - startTime < ms);
+    console.log('sleep done');
+}
+
+const btn = document.querySelector('button');
+btn.addEventListener('click', function(){
+    console.log('button clicked');
+})
+
+setTimeout(function(){
+    sleep(3000);
+}, 2000);
+
+// sleep(3000);
