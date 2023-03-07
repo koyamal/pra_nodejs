abad0ceb98fc883d9fd17738edbdee118b303dc5
koyamal <tanakamaruko1121n@gmail.com>
2023-03-07 16:24:38 +0900

    learn 130

--- use_js/browser/2023/130/main.js
+++ use_js/browser/2023/130/main.js
@@ -5,8 +5,19 @@ new Promise(function promies(resolve){
         console.log('task1');
         resolve();
     });
+
+    setTimeout(function task3(){
+        console.log('task3');
+    })
 }).then(function job1(){
     console.log('job1');
+    setTimeout(function task2(){
+        console.log('task2');
+    })
+}).then(function job2(){
+    console.log('job2');
+}).then(function jo3(){
+    console.log('job3');
 });
 
 console.log('global end');
