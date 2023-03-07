77adc7107cbb9bd0c0008586b9b9721dde5322e9
koyamal <tanakamaruko1121n@gmail.com>
2023-03-07 13:42:15 +0900

    learn 125

---
+++ use_js/browser/2023/125/main.js
@@ -0,0 +1,12 @@
+function sleep(callback, val){
+    setTimeout(function(){
+        console.log(val++);
+        callback(val);
+    }, 1000);
+}
+
+sleep(function(val){
+    sleep(function(val){
+
+    }, val);
+}, 0)
