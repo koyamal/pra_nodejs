adea1368fa838908ace7807377b35f4c32449e11
koyamal <tanakamaruko1121n@gmail.com>
2023-03-09 08:01:46 +0900

    learn 138

---
+++ use_js/browser/2023/138/moduleA.js
@@ -0,0 +1,7 @@
+export let publicVal = 0;
+
+export function publicFn(){
+    console.log('publicFn is called');
+}
+
+export default 1;
---
+++ use_js/browser/2023/138/moduleB.js
@@ -0,0 +1,6 @@
+import defaultVal, { publicVal as val, publicFn } from "./moduleA.js";
+
+// console.log(publicVal);
+console.log(val);
+console.log(defaultVal);
+publicFn();
