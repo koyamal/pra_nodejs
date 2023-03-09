ecf659c1344f82edf431aa2489054d24de0c8374
koyamal <tanakamaruko1121n@gmail.com>
2023-03-10 08:31:33 +0900

    learn 144

--- use_js/browser/2023/144/moduleB.js
+++ use_js/browser/2023/144/moduleB.js
@@ -2,7 +2,15 @@
 
 // publicFn();
 
-import('./moduleA.js').then(function(modules){
+// import('./moduleA.js').then(function(modules){
+//     console.log(modules);
+//     modules.publicFn();
+// })
+
+async function fn(){
+    const modules = await import('./moduleA.js');
     console.log(modules);
     modules.publicFn();
-})
+}
+
+fn();
