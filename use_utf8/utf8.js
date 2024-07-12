5d68805eeaa3af5eb13171f58df28ebd52d958f5
koyamal <tanakamaruko1121n@gmail.com>
2024-07-12 17:57:06 +0900

    コンソールに出力

--- use_utf8/utf8.js
+++ use_utf8/utf8.js
@@ -3,3 +3,6 @@ const encoder = new TextEncoder();
 const decoder = new TextDecoder();
 const encodedText = encoder.encode(originText);
 const decodedText = decoder.decode(encodedText);
+
+console.log("encode", encodedText);
+console.log("decode", decodedText);
