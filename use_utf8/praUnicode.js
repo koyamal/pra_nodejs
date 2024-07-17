de2314f7f1cf7822e23bdb6f6b0694325024406f
koyamal <tanakamaruko1121n@gmail.com>
2024-07-17 10:15:51 +0900

    コメントを編集

--- use_utf8/praUnicode.js
+++ use_utf8/praUnicode.js
@@ -1,8 +1,16 @@
 /**
  * unicodeのコードポイントをutf-8へ変換する
+ * 
+ * 流れ：
+ * (文字列をunicodeのコードポイントへ変換する)
+ * unicodeのコードポイントを2進数に変換する
+ * 2進数を4,6,6ビットに分割する
+ * 分割した各値に定数(e0,80,80)を加える
+ * 16進数に変換する
  */
 const Encoding = require('encoding-japanese');
 
+// utf-8へ変換したい文字列
 const orignText = 'おに';
 
 // 文字列をunicode(10進数)へ変換
@@ -17,9 +25,7 @@ console.log(`"${orignText}"のunicode(16進数): `, unicodeArrayHex);
 const unicodeArrayBinary = unicodeArray.map(unicodeNumber => unicodeNumber.toString(2));
 console.log(`"${orignText}"のunicode(2進数): `, unicodeArrayBinary);
 
-const textBinary = unicodeArrayBinary[0];
-
-// 先頭から4(値によっては3桁とか2桁とかになるかも),6,6ビットに分割する
+// 先頭から4(値によっては3桁とか2桁とかになるかも),6,6ビットに分割する utf-8への変換方法で決まってる。
 const dividedUnicodeArrayBinary = unicodeArrayBinary.map((unicodeBinary) => {
   const first = unicodeBinary.slice(0, unicodeBinary.length -12);
   const second = unicodeBinary.slice(unicodeBinary.length -12, unicodeBinary.length -6);
@@ -27,20 +33,16 @@ const dividedUnicodeArrayBinary = unicodeArrayBinary.map((unicodeBinary) => {
   return [first, second, third];
 });
 
-const first = textBinary.slice(0, textBinary.length -12);
-const second = textBinary.slice(textBinary.length -12, textBinary.length -6);
-const third = textBinary.slice(textBinary.length -6);
-console.log(first, second, third);
-
-// utf-8へ変換するために分割した各値に定数(e0, 80, 80)を加える
+// unicodeからutf-8へ変換するために分割した各値に定数(e0, 80, 80)を加えるためにバイナリに変換
 const addNumberHex = ['e0', '80', '80'];
 const addNumberBinary = addNumberHex.map(hex => parseInt(hex, 16).toString(2));
-console.log(addNumberBinary);
 
+// 2個のバイナリ(文字列)の和を16進数で返す
 const calBinaryReturnHex = (a, b) => {
   return (BigInt(`0b${a}`) + BigInt(`0b${b}`)).toString(16);
 }
 
+// unicodeからutf-8へ変換するために分割した各値に定数(e0, 80, 80)のバイナリを加える
 const utf8ArrayHex = dividedUnicodeArrayBinary.map((dividedUnicodeBinary) => {
   const first = calBinaryReturnHex(dividedUnicodeBinary[0], addNumberBinary[0]);
   const second = calBinaryReturnHex(dividedUnicodeBinary[1], addNumberBinary[1]);
@@ -49,8 +51,4 @@ const utf8ArrayHex = dividedUnicodeArrayBinary.map((dividedUnicodeBinary) => {
   return first + second + third;
 });
 
-console.log("xxxxx",utf8ArrayHex);
-
-console.log(calBinaryReturnHex('0011', '11100000'));
-console.log(calBinaryReturnHex('000001', '10000000'));
-console.log(calBinaryReturnHex('001010', '10000000'));
+console.log(`"${orignText}"のutf-8(16進数): `, utf8ArrayHex);
