a2e7ba857b06ef1c5ca5b2cdc0392a9d77f96613
koyamal <tanakamaruko1121n@gmail.com>
2024-07-24 08:17:44 +0900

    sha256でhash

--- use_hash/pra_hash.js
+++ use_hash/pra_hash.js
@@ -1,7 +1,8 @@
 const crypto = require('crypto');
 
 // console.log(crypto.getHashes());
-
-const sha256Hash = crypto.createHash('sha256').update("plainText").digest('hex');
-console.log(sha256Hash);
+const plainText = "helloWorld";
+console.log(`plain text: ${plainText}`);
+const sha256Hash = crypto.createHash('sha256').update(plainText).digest('hex');
+console.log(`sha256: ${sha256Hash}`);
 console.log(`${sha256Hash.length}文字`);
