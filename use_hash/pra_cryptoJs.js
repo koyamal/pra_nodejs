24779f4dfbcf1fd78e4ff18872c49811fb9f84f9
koyamal <tanakamaruko1121n@gmail.com>
2024-07-26 09:49:32 +0900

    関数を追加

--- use_hash/pra_cryptoJs.js
+++ use_hash/pra_cryptoJs.js
@@ -2,11 +2,15 @@ const CryptoJS = require('crypto-js');
 
 const originText = 'helloWorld';
 
+const consoleHash = (type, msg, hashHex) => {
+  console.log(`${type}(${msg}): ${hashHex} (${hashHex.length})`);
+}
+
 const toHashSha256 = (msg, flag=false) => {
   const hash = CryptoJS.SHA256(msg);
   const hashHex = hash.toString(CryptoJS.enc.Hex);
-  if (flag) console.log(`sha-256(${msg}): ${hashHex} (${hashHex.length})`);
+  if (flag) consoleHash('sha-256', msg, hashHex);
   return hashHex;
 }
 
-console.log(toHashSha256(originText, true));
+toHashSha256(originText, true);
