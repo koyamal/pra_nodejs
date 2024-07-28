d27b25dc3c1fac418ddef4800bb5cb5dd451bc98
koyamal <tanakamaruko1121n@gmail.com>
2024-07-28 09:30:00 +0900

    復号化

--- use_ciphers/pra_cryptoJs.js
+++ use_ciphers/pra_cryptoJs.js
@@ -4,3 +4,5 @@ const originText = 'helloWorld';
 
 const encrypted = CryptoJS.AES.encrypt(originText, "hello");
 console.log(`${originText} to ${encrypted}`);
+
+const decrypted = CryptoJS.AES.decrypt(encrypted, "hello");
