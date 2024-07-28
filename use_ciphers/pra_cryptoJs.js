6dc029f6b22d200f3696dbacf4fa0ad611c6d259
koyamal <tanakamaruko1121n@gmail.com>
2024-07-28 09:52:56 +0900

    keyやivを出力

--- use_ciphers/pra_cryptoJs.js
+++ use_ciphers/pra_cryptoJs.js
@@ -3,7 +3,13 @@ const CryptoJS = require('crypto-js');
 const originText = 'helloWorld';
 
 const encrypted = CryptoJS.AES.encrypt(originText, "hello");
+console.log('=======================');
 console.log(`${originText} to ${encrypted}`);
+console.log(`key: ${encrypted.key}`);
+console.log(`iv: ${encrypted.iv}`);
+console.log(`salt: ${encrypted.salt}`);
+console.log(`ciphertext: ${encrypted.ciphertext}`);
+console.log('=======================');
 
 const decrypted = CryptoJS.AES.decrypt(encrypted, "hello");
 console.log(`decrypted: ${decrypted}`); // utf-8
