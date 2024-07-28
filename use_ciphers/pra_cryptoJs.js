53d4ea5370abf9d578e96a47e53c7b9cbfb75422
koyamal <tanakamaruko1121n@gmail.com>
2024-07-28 09:33:07 +0900

    複合化を出力

--- use_ciphers/pra_cryptoJs.js
+++ use_ciphers/pra_cryptoJs.js
@@ -6,3 +6,5 @@ const encrypted = CryptoJS.AES.encrypt(originText, "hello");
 console.log(`${originText} to ${encrypted}`);
 
 const decrypted = CryptoJS.AES.decrypt(encrypted, "hello");
+console.log(decrypted);
+console.log(decrypted.toString(CryptoJS.enc.Utf8));
