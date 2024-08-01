b3bb45e4be988100e07437b571bb111055a92aaf
koyamal <tanakamaruko1121n@gmail.com>
2024-08-01 22:14:01 +0900

    修正

--- use_hash/pra_hash.js
+++ use_hash/pra_hash.js
@@ -2,11 +2,11 @@ const crypto = require('crypto');
 
 // console.log(crypto.getHashes());
 const plainText = "helloWorld";
-console.log(`plain text: ${plainText}`);
+console.log(`plain text===>: ${plainText}`);
 const sha256Hash = crypto.createHash('sha256').update(plainText).digest('hex');
-console.log(`sha256: ${sha256Hash} (${sha256Hash.length}文字)`);
+console.log(`sha256===>: ${sha256Hash} (${sha256Hash.length}文字)`);
 
-const plainTexts = "helloworld";
+const plainTexts = "helloworldXXXX";
 console.log(`plain text: ${plainTexts}`);
 const sha256Hashs = crypto.createHash('sha256').update(plainTexts).digest('hex');
 console.log(`sha256: ${sha256Hashs} (${sha256Hashs.length}文字)`);
