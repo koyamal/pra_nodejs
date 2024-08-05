43efaba16727997df1506eb24ccd06b5ff5ca5ed
koyamal <tanakamaruko1121n@gmail.com>
2024-08-05 18:27:19 +0900

    DESを利用

---
+++ use_ciphers/pra_cryptoDES.js
@@ -0,0 +1,15 @@
+const CryptoJS = require('crypto-js');
+
+const originText = 'goodBy';
+
+const encrypted = CryptoJS.DES.encrypt(originText, "bye");
+console.log('=======================');
+console.log(`${originText} to ${encrypted}`);
+console.log(`key: ${encrypted.key}`);
+console.log(`iv: ${encrypted.iv}`);
+console.log(`salt: ${encrypted.salt}`);
+console.log(`ciphertext: ${encrypted.ciphertext}`);
+console.log('=======================');
+
+const decrypted = CryptoJS.DES.decrypt(encrypted, "bye");
+console.log(`decrypted: ${decrypted.toString(CryptoJS.enc.Utf8)}`); // utf-8
