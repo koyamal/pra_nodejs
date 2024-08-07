c2acaf186a1a0c7fa6447ed8bd5ce91ec532fda9
koyamal <tanakamaruko1121n@gmail.com>
2024-08-07 22:16:04 +0900

    修正

---
+++ use_hash/pra_hash_check.js
@@ -0,0 +1,12 @@
+const crypto = require('crypto');
+
+// console.log(crypto.getHashes());
+const plainText = "helloWorld";
+console.log(`plain text===>: ${plainText}`);
+const sha256Hash = crypto.createHash('sha256').update(plainText).digest('hex');
+console.log(`sha256===>: ${sha256Hash} (${sha256Hash.length}文字)`);
+
+const plainTexts = "helloworldXXXX";
+console.log(`plain text: ${plainTexts}`);
+const sha256Hashscheck = crypto.createHash('sha256').update(plainTexts).digest('hex');
+console.log(`sha256: ${sha256Hashscheck} (${sha256Hashscheck.length}文字)`);
