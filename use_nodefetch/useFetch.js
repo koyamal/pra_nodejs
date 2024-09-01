abf9d406d7a3439ead14e81a15fe14263dc68f87
koyamal <74910401+koyamal@users.noreply.github.com>
2024-09-01 17:23:27 +0900

    修正

--- use_nodefetch/useFetch.js
+++ use_nodefetch/useFetch.js
@@ -2,14 +2,15 @@ import fetch from 'node-fetch'
 
 const responses = await Promise.all([
   await fetch('https://github.com/'),
-  await fetch('https://yahoo.co.jp/')
+  await fetch('https://yahoo.co.jp/'),
+  await fetch('https://sdkfaswaera.fdsfsfs')
 ]);
 
 console.log(responses);
 
 const responsesAllSettled = await Promise.allSettled([
   await fetch('https://github.com/'),
-  await fetch('https://yahoo.co.jp/')
+  await fetch('https://yahoo.co.jp/'),
 ]);
 
 console.log(responsesAllSettled);
