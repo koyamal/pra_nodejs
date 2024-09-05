386c5455f5ab4dd5539848ee076a869be17fa299
koyamal <74910401+koyamal@users.noreply.github.com>
2024-09-05 14:46:47 +0900

    create

---
+++ use_trim/trim.js
@@ -0,0 +1,9 @@
+const originText = ' a b c ';
+const output = originText.trim();
+console.log(output);
+
+const output2 = originText.trimStart();
+console.log(output2);
+
+const output3 = originText.trimEnd();
+console.log(output3);
