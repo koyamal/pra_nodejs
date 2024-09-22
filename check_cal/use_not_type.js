f625b7334688c259f44f5d871f2df656d7a9401f
koyamal <74910401+koyamal@users.noreply.github.com>
2024-09-22 18:03:58 +0900

    修正

--- check_cal/use_not_type.js
+++ check_cal/use_not_type.js
@@ -2,4 +2,7 @@
 // const messageText2: string | undefined = undefined;
 const messageText2 = "haelloWorld";
 console.log(messageText2.slice(1, 2));
-// console.log(messageText2.slice(1, 2));
+const bookA = {
+    bookName: '猫飼ってみた'
+};
+console.log(bookA.bookName);
--- check_cal/use_not_type.ts
+++ check_cal/use_not_type.ts
@@ -3,3 +3,13 @@ const messageText2: string | undefined = "haelloWorld";
 
 console.log(messageText2!.slice(1, 2));
 // console.log(messageText2.slice(1, 2));
+
+interface books {
+  bookName: string;
+}
+
+const bookA: books = {
+  bookName: '猫飼ってみた'
+}
+
+console.log(bookA.bookName);
