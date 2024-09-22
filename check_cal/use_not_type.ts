8a8a065d5ee9b753847fc754d105145313b1ad1e
koyamal <74910401+koyamal@users.noreply.github.com>
2024-09-22 17:30:25 +0900

    修正

--- check_cal/use_not_type.ts
+++ check_cal/use_not_type.ts
@@ -1,4 +1,5 @@
 const messageText2: string | undefined = undefined;
+const messageText2: string | undefined = "helloWorld";
 
 console.log(messageText2!.slice(1, 2));
-console.log(messageText2.slice(1, 2));
+// console.log(messageText2.slice(1, 2));
