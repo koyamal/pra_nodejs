b9ed83cc000a0d04c60aeddffab8a56e7824b217
koyamal <74910401+koyamal@users.noreply.github.com>
2024-09-22 17:33:04 +0900

    修正

--- check_cal/use_not_type.js
+++ check_cal/use_not_type.js
@@ -1,2 +1,5 @@
-var messageText2 = undefined;
+"use strict";
+// const messageText2: string | undefined = undefined;
+const messageText2 = "haelloWorld";
 console.log(messageText2.slice(1, 2));
+// console.log(messageText2.slice(1, 2));
--- check_cal/use_not_type.ts
+++ check_cal/use_not_type.ts
@@ -1,5 +1,5 @@
-const messageText2: string | undefined = undefined;
-const messageText2: string | undefined = "helloWorld";
+// const messageText2: string | undefined = undefined;
+const messageText2: string | undefined = "haelloWorld";
 
 console.log(messageText2!.slice(1, 2));
 // console.log(messageText2.slice(1, 2));
