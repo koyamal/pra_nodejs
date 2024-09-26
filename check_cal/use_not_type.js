3e5ae14be12b69ebe411de6628b006a606512623
koyamal <74910401+koyamal@users.noreply.github.com>
2024-09-26 23:15:11 +0900

    修正

--- check_cal/use_not_type.js
+++ check_cal/use_not_type.js
@@ -1,6 +1,6 @@
 "use strict";
 const envTableName = 'is_this_table';
-const envFlag = false;
+const envFlag = true;
 const outputText = `Output Text is ${envFlag ? envTableName : 'no_table'}.`;
 console.log(outputText);
 // const messageText2: string | undefined = undefined;
@@ -8,6 +8,7 @@ const messageText2 = "haelloWorld";
 console.log(messageText2.slice(1, 2));
 const bookA = {
     bookName: '猫飼ってみた',
-    author: '冬肘掃除'
+    author: '冬肘掃除',
+    price: 498,
 };
 console.log(bookA.bookName);
