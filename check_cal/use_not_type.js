1b86ea7c50168284003eaef8dd89849c9ad1d945
koyamal <74910401+koyamal@users.noreply.github.com>
2024-09-27 18:40:09 +0900

    修正

--- check_cal/use_not_type.js
+++ check_cal/use_not_type.js
@@ -1,4 +1,6 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
 const envTableName = 'is_this_table';
 const envFlag = true;
 const outputText = `Output Text is ${envFlag ? envTableName : 'no_table'}.`;
@@ -10,5 +12,6 @@ const bookA = {
     bookName: '猫飼ってみた',
     author: '冬肘掃除',
     price: 498,
+    bookId: (0, uuid_1.v4)()
 };
 console.log(bookA.bookName);
