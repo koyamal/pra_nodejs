b3eb7808e3436b2dc9b73e1e8874b498ca1b2313
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-06 18:48:58 +0900

    修正

--- use_dayjs/dayjs.ts
+++ use_dayjs/dayjs.ts
@@ -4,7 +4,7 @@ import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(timezone);
 dayjs.extend(DayJSUtc);
-console.log(dayjs());
 
+const nowNoTz = dayjs();
 const nowTokyo = dayjs().tz("Asia/Tokyo");
 console.log(nowTokyo);
