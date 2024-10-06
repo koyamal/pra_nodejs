20ae4d5f00fad1ccbfa17e392e3474c7ed5a844e
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-06 18:39:37 +0900

    修正

--- use_dayjs/dayjs.js
+++ use_dayjs/dayjs.js
@@ -4,7 +4,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const dayjs_1 = __importDefault(require("dayjs"));
+const utc_1 = __importDefault(require("dayjs/plugin/utc"));
 const timezone_1 = __importDefault(require("dayjs/plugin/timezone"));
 dayjs_1.default.extend(timezone_1.default);
+dayjs_1.default.extend(utc_1.default);
 console.log((0, dayjs_1.default)());
 (0, dayjs_1.default)().tz("Asia/Tokyo");
--- use_dayjs/dayjs.ts
+++ use_dayjs/dayjs.ts
@@ -1,7 +1,9 @@
 import dayjs from 'dayjs';
+import DayJSUtc from 'dayjs/plugin/utc'
 import timezone from 'dayjs/plugin/timezone';
 
 dayjs.extend(timezone);
+dayjs.extend(DayJSUtc);
 console.log(dayjs());
 
 dayjs().tz("Asia/Tokyo")
