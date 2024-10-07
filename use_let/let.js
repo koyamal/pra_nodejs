954441e02d338e7ac0adb6834578052e5db17df7
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-08 08:02:47 +0900

    修正

--- use_let/let.js
+++ use_let/let.js
@@ -1,13 +1,10 @@
+"use strict";
 const dataBox = [1, 2, 3];
-
-// constの配列に要素を追加していく。
 const constBox = [];
 dataBox.forEach(data => {
     constBox.push(data);
 });
 console.log(constBox);
-
-// letの配列に要素を追加していく。
 let letBox = [];
 dataBox.forEach(data => {
     letBox.push(data);
