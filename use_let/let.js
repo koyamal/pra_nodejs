79492d63898242974c12c5d0e2d9ef6b3d83e4c4
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-08 08:01:57 +0900

    修正

--- use_let/let.js
+++ use_let/let.js
@@ -1,9 +1,11 @@
 const dataBox = [1, 2, 3];
+
 const constBox = [];
 dataBox.forEach(data => {
     constBox.push(data);
 });
 console.log(constBox);
+
 let letBox = [];
 dataBox.forEach(data => {
     letBox.push(data);
