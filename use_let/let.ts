d54e91f03ad51409f97162df103759348b2f1f31
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-10 08:06:15 +0900

    修正

--- use_let/let.ts
+++ use_let/let.ts
@@ -28,7 +28,7 @@ const jsonBox: JsonBox[] = [
   {date: "2025-01-01T00:00:00Z", id: 2},
 ];
 
-function compareById( a: JsonBox , b: JsonBox ){
+function compareByDate( a: JsonBox , b: JsonBox ){
   var r = 0;
   if( dayjs(a.date) < dayjs(b.date) ){ r = -1; }
   else if( dayjs(a.date) > dayjs(a.date) ){ r = 1; }
@@ -36,7 +36,7 @@ function compareById( a: JsonBox , b: JsonBox ){
   return r;
 }
 
-function compareBydate( a: JsonBox , b: JsonBox ){
+function compareById( a: JsonBox , b: JsonBox ){
   var r = 0;
   if( a.id < b.id ){ r = -1; }
   else if( a.id > b.id ){ r = 1; }
