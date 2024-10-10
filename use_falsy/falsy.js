ef5d60dbb0226e874ead5734616082f1427c6d4e
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-10 19:49:12 +0900

    修正

---
+++ use_falsy/falsy.js
@@ -0,0 +1,15 @@
+"use strict";
+const val1 = undefined;
+const val2 = [];
+if (val1) {
+    console.log("val1はTruthy");
+}
+else {
+    console.log("val1はFalsy");
+}
+if (val2) {
+    console.log("val2はTruthy");
+}
+else {
+    console.log("val2はFalsy");
+}
