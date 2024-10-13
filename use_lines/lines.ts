4b13302b58f50e076870c9c10163e2da84465fbc
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-13 18:05:51 +0900

    修正

--- use_lines/lines.ts
+++ use_lines/lines.ts
@@ -1,5 +1,11 @@
 const espLines = "hello,\nworld.";
 const espTabs = "hello,\tworld.";
+const userName = "Bob";
 
 console.log(espLines);
 console.log(espTabs);
+
+const textLines = [
+  "こんにちは。",
+  `私の名前は${userName}です。`,
+].join('\n');
