2e79b0bfce6b82501fdc234ced773a7dc8630fa2
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-13 14:42:35 +0900

    修正

--- use_falsy/falsy.js
+++ use_falsy/falsy.js
@@ -22,12 +22,6 @@ console.log(val2 !== null && val2 !== void 0 ? val2 : `${val2} ?? X => X`);
 console.log(val3 !== null && val3 !== void 0 ? val3 : `${val3} ?? X => X`);
 console.log(val4 !== null && val4 !== void 0 ? val4 : `${val4} ?? X => X`);
 console.log(val5 !== null && val5 !== void 0 ? val5 : `${val5} ?? X => X`);
-if (val1 !== undefined) {
-    console.log(`${val1}はundefinedではありません。`);
-}
-else {
-    console.log(`${val1}はundefinedです。`);
-}
 valBox.forEach(val => {
     if (val !== undefined) {
         console.log(`${val}はundefinedではありません。`);
