c8bb96fb039e75573bd4698712635a74a2f31064
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-15 08:18:58 +0900

    修正usus

--- use_lines/lines.js
+++ use_lines/lines.js
@@ -13,3 +13,12 @@ const textLines = [
     "頑張ります。"
 ].join('\n');
 console.log(textLines);
+const textTabs = [
+    "こんにちは。",
+    `私の名前は${userName}です。`,
+    "",
+    "よろしくお願いします。",
+    "",
+    "頑張ります。"
+].join('\t');
+console.log(textTabs);
