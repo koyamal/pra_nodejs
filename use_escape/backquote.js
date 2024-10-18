e509ba35b525233df30f33c40691b8163d292764
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-18 17:55:53 +0900

    修正

--- use_escape/backquote.js
+++ use_escape/backquote.js
@@ -7,3 +7,10 @@ const textOrigin = [
     "頑張ります。"
 ].join('');
 console.log("textOrigin", textOrigin);
+const textBackQuote = [
+    "こんにちは。",
+    `私の名前はBobです。`,
+    "よろしくお願いします。",
+    "頑張ります。"
+].join('\`');
+console.log("textBackQuote", textBackQuote);
