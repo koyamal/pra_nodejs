c1f4426558b92595d2d00b2f84c46f72abd78c25
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-18 17:54:04 +0900

    削除

---
+++ use_escape/backquote.ts
@@ -0,0 +1,10 @@
+const textOrigin = [
+  "こんにちは。",
+  `私の名前はBobです。`,
+  "よろしくお願いします。",
+  "頑張ります。"
+].join('');
+
+console.log("textOrigin", textOrigin);
+
+export {};
--- use_escape/escape.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const textOrigin = [
-    "こんにちは。",
-    `私の名前はBobです。`,
-    "よろしくお願いします。",
-    "頑張ります。"
-].join('');
-console.log("textOrigin", textOrigin);
