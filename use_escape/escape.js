121fbafb68f4fa0f98206cb0c8f1c270b06bafae
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-18 17:51:37 +0900

    修正

---
+++ use_escape/escape.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const textOrigin = [
+    "こんにちは。",
+    `私の名前は${userName}です。`,
+    "よろしくお願いします。",
+    "頑張ります。"
+].join('');
+console.log(textOrigin);
