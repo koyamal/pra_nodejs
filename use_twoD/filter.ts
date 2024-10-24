c8d68d0835e85d11520a438d2812ec34442cf22d
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-24 23:25:04 +0900

    修正

--- use_twoD/filter.ts
+++ use_twoD/filter.ts
@@ -1,10 +1,13 @@
 const items = [...Array(50)].map((_, i) => {
+  if(i % 7 === 0) return "";
   return i.toString();
 });
 
 items.push("");
 
+console.log("items", items);
+
 const filteredItems = items.filter(item => item);
-console.log(filteredItems);
+console.log("filteredItems", filteredItems);
 
 export {}
