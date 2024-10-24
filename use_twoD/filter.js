0e1ad5402a869fc1290af1599b0c51f9db2f8653
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-24 23:24:09 +0900

    修正

--- use_twoD/filter.js
+++ use_twoD/filter.js
@@ -1,7 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const items = [...Array(50)].map((_, i) => {
-    return i;
+    return i.toString();
 });
+items.push("");
 const filteredItems = items.filter(item => item);
 console.log(filteredItems);
--- use_twoD/filter.ts
+++ use_twoD/filter.ts
@@ -2,6 +2,8 @@ const items = [...Array(50)].map((_, i) => {
   return i.toString();
 });
 
+items.push("");
+
 const filteredItems = items.filter(item => item);
 console.log(filteredItems);
 
