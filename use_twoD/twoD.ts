34e326637d3cd04de343de1ae30db911b1a01cca
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-24 17:10:52 +0900

    修正

--- use_twoD/twoD.ts
+++ use_twoD/twoD.ts
@@ -1,4 +1,4 @@
-const getTwoDArray = (items: string[]): string[][] => {
+const getTwoDArray = (items: Number[]): Number[][] => {
   const twoDArray = [];
   for (let i = 0; i < items.length; i += 20) {
     twoDArray.push(items.slice(i, i + 20));
@@ -9,5 +9,7 @@ const getTwoDArray = (items: string[]): string[][] => {
 const items = [...Array(50)].map((_, i) => {
   return i;
 });
+
+const twoDArray = getTwoDArray(items);
 
 console.log(items);
