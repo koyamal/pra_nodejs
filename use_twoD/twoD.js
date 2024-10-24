221542cfa8e2c63bea255356ea0c9bb2e3dc52bc
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-24 17:09:58 +0900

    作成

---
+++ use_twoD/twoD.js
@@ -0,0 +1,12 @@
+"use strict";
+const getTwoDArray = (items) => {
+    const twoDArray = [];
+    for (let i = 0; i < items.length; i += 20) {
+        twoDArray.push(items.slice(i, i + 20));
+    }
+    return twoDArray;
+};
+const items = [...Array(50)].map((_, i) => {
+    return i;
+});
+console.log(items);
