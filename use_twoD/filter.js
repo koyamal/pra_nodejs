41791615665b9be501db63898ab894dbc297bad5
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-25 21:05:03 +0900

    修正

--- use_twoD/filter.js
+++ use_twoD/filter.js
@@ -9,5 +9,7 @@ items.push(0);
 console.log("items", items);
 const filteredItems = items.filter(item => item);
 console.log("filteredItems", filteredItems);
-const filteredMapedItems = items.filter(item => item % 8 !== 0).map((notSevenEightValue) => { notSevenEightValue; });
+const filteredMapedItems = items.filter(item => item % 8 !== 0).map((notSevenEightValue) => {
+    return { notSevenEightValue };
+});
 console.log(filteredMapedItems);
