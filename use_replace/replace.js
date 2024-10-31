dbdd3f690cb0ebb4bc4aa4b49f955cbe47d5d0fe
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-31 21:26:33 +0900

    修正

--- use_replace/replace.js
+++ use_replace/replace.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const replaceText = (text) => {
-    return text.replace(/(and||or)%20/, '$1a ');
+    return text.replace(/(and||or)%20/, 'a');
 };
 console.log(replaceText("and 123"));
