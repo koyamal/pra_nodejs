6fdce2bb459b39b9f2be1074df28869edc9dec09
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-31 21:26:26 +0900

    修正

--- use_replace/replace.ts
+++ use_replace/replace.ts
@@ -1,5 +1,5 @@
 const replaceText = (text: string): string => {
-  return text.replace(/(and||or)%20/, '$1a ');
+  return text.replace(/(and||or)%20/, 'a');
 }
 
 console.log(replaceText("and 123"));
