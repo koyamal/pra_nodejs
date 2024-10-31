ce3aacc85bf3a51483caabd7e5129384019a0369
koyamal <74910401+koyamal@users.noreply.github.com>
2024-10-31 21:23:24 +0900

    修正

--- use_replace/replace.js
+++ use_replace/replace.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const replaceText = (text) => {
-    return text.replace(/(and||or)/, '¥1 ');
+    return text.replace(/(and||or)/, '$1 ');
 };
 console.log(replaceText("and 123"));
--- use_replace/replace.ts
+++ use_replace/replace.ts
@@ -1,5 +1,5 @@
 const replaceText = (text: string): string => {
-  return text.replace(/(and||or)/, '$1 ');
+  return text.replace(/(and||or)%20/, '$1 ');
 }
 
 console.log(replaceText("and 123"));
