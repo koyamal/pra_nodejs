015c08dffaabfe6a30f88ac3a7f59712179ecb45
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-01 08:25:29 +0900

    修正

--- use_replace/replace.js
+++ use_replace/replace.js
@@ -4,5 +4,19 @@ const replaceText = (text) => {
     return text.replace(/(and|or)\x20/, '$1 ');
 };
 const originText = "and 123";
+const replacedText = replaceText(originText);
 console.log(originText);
-console.log(replaceText(originText));
+console.log(replacedText);
+function convertCodeUnits(str) {
+    const codeUnits = [];
+    for (let i = 0; i < str.length; i++) {
+        codeUnits.push(str.charCodeAt(i).toString(16));
+    }
+    return codeUnits;
+}
+const convertOriginText = convertCodeUnits(originText);
+const convertReplacedText = convertCodeUnits(replacedText);
+console.log("originText: ", originText);
+console.log(convertOriginText);
+console.log("replacedText: ", replacedText);
+console.log(convertReplacedText);
