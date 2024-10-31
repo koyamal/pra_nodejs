08cd44cbf10213dbf92dcb925e644de1514203a0
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-01 08:23:05 +0900

    修正

--- use_replace/replace.ts
+++ use_replace/replace.ts
@@ -2,8 +2,18 @@ const replaceText = (text: string): string => {
   return text.replace(/(and|or)\x20/, '$1 ');
 }
 
-const originText = "and 123"
+const originText = "and 123";
 console.log(originText);
 console.log(replaceText(originText));
+
+function convertCodeUnits(str: string) {
+  const codeUnits = [];
+  for (let i = 0; i < str.length; i++) {
+      codeUnits.push(str.charCodeAt(i).toString(16));
+  }
+  return codeUnits;
+}
+
+const convertReplacedText = ""
 
 export {}
