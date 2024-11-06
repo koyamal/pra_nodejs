a569d0c8e7c9b7978d0934d069aef7ef661a5f98
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-06 20:50:19 +0900

    修正

--- use_filter/filter.ts
+++ use_filter/filter.ts
@@ -3,5 +3,5 @@ const numberBox = [1, 3, 4, 4, 7, 6, 11, 65, 23, 1, 5, 8];
 const filteredBox = numberBox.filter(number => number % 7 === 0);
 console.log(filteredBox);
 
-const filteredMappedBox = numberBox.filter(number => number % 2 === 0).map(number => {value: number});
+const filteredMappedBox = numberBox.filter(number => number % 2 === 0).map(number => { return {value: number}});
 console.log(filteredMappedBox);
