07d56269644dedbc109176ac5694a175bd2cc384
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-06 20:52:55 +0900

    修正

--- use_filter/filter.ts
+++ use_filter/filter.ts
@@ -1,4 +1,4 @@
-const numberBox = [1, 3, 4, 4, 7, 6, 11, 65, 23, 1, 5, 8];
+const numberBox: number[] = [1, 3, 4, 4, 7, 6, 11, 65, 23, 1, 5, 8];
 
 const filteredBox = numberBox.filter(number => number % 7 === 0);
 console.log(filteredBox);
