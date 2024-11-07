8a48776e5f6428e49b02591acdaae2e47af29b50
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-07 23:02:58 +0900

    修正

--- use_filter/filter.js
+++ use_filter/filter.js
@@ -4,3 +4,5 @@ const filteredBox = numberBox.filter(number => number % 7 === 0);
 console.log(filteredBox);
 const filteredMappedBox = numberBox.filter(number => number % 2 === 0).map(number => { return { valueNumber: number }; });
 console.log(filteredMappedBox);
+const MappedfilteredBox = numberBox.map(number => { return { valueNumber: number }; }).filter(val => val.valueNumber >= 6);
+console.log(MappedfilteredBox);
