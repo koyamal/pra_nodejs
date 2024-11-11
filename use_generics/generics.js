c231787b14eb9ed7d7962d9829470bb0d6b727e6
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-11 20:58:07 +0900

    修正

--- use_generics/generics.js
+++ use_generics/generics.js
@@ -2,11 +2,12 @@
 const test = (arg) => {
     return arg;
 };
-const test2 = (arg) => {
+const test2 = (arg, unit) => {
+    console.log(unit);
     return arg.name;
 };
 console.log(test(1));
 console.log(test("hello"));
 console.log(test(1));
 console.log(test("hello"));
-console.log(test2({ name: "name", age: 23 }));
+console.log(test2({ name: "name", age: 23 }, "hello"));
