b4e6a0ca206db39d4f59801f6e76feb705f7cad7
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-13 17:30:05 +0900

    修正

--- use_dayjs/second.js
+++ use_dayjs/second.js
@@ -6,15 +6,23 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dayjs_1 = __importDefault(require("dayjs"));
 const utc_1 = __importDefault(require("dayjs/plugin/utc"));
 dayjs_1.default.extend(utc_1.default);
-const cT = () => {
-    const currentUtc = (0, dayjs_1.default)().utc();
-    return currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm:ss');
+const roundSecond = (rowSecond, unitValue) => {
+    const remainder = rowSecond % unitValue;
+    if (remainder === 0) {
+        return rowSecond;
+    }
+    else {
+        return rowSecond + (unitValue - remainder);
+    }
 };
-console.log(cT());
 const amari = (val) => {
     const val2 = (0, dayjs_1.default)().second();
     console.log(val2);
     const amari = val2 % 5;
     return val2 + (5 - amari);
 };
-console.log(amari(12));
+const cT = () => {
+    const currentUtc = (0, dayjs_1.default)().utc();
+    return currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm') + ":" + roundSecond(currentUtc.second(), 5);
+};
+console.log(cT());
