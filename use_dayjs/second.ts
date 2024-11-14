5c8058a65ca3a6b6c519437bca859590c546a4e7
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-15 08:06:37 +0900

    修正

--- use_dayjs/second.ts
+++ use_dayjs/second.ts
@@ -3,15 +3,15 @@ import utc from "dayjs/plugin/utc"
 
 dayjs.extend(utc);
 
-const roundSecond = (rowSecond: number, unitValue: number) => {
+const roundUpSecond = (rowSecond: number, unitValue: number) => {
   const remainder = rowSecond % unitValue;
   return rowSecond + (unitValue - remainder);
 }
 
-const cT = () => {
+const ceilTimestamp = () => {
   const currentUtc = dayjs().utc();
   const currentUtcString = currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm');
-  return dayjs(currentUtcString).add(roundSecond(currentUtc.second(), 5), 's').format('YYYY-MM-DD HH:mm:ss');
+  return dayjs(currentUtcString).add(roundUpSecond(currentUtc.second(), 5), 's').format('YYYY-MM-DD HH:mm:ss');
 }
 
-console.log(cT());
+console.log(ceilTimestamp());
