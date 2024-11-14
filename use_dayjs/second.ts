fcd350e8a8afa47d401f82e1b9b587d31b1146af
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-14 22:30:51 +0900

    修正

--- use_dayjs/second.ts
+++ use_dayjs/second.ts
@@ -3,10 +3,17 @@ import utc from "dayjs/plugin/utc"
 
 dayjs.extend(utc);
 
+
+
 const roundSecond = (rowSecond: number, unitValue: number, das: string) => {
   const remainder = rowSecond % unitValue;
+
+  console.log("e", dayjs(das).add(rowSecond + (unitValue - remainder), 's').format('YYYY-MM-DD HH:mm:ss'))
+  // return dayjs(das).add(rowSecond + (unitValue - remainder), 's')
   return rowSecond + (unitValue - remainder);
+
 }
+
 
 const cT = () => {
   const currentUtc = dayjs().utc();
