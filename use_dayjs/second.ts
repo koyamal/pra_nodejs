34e5ecd79c4bc317f51f8261b340c24e7ce3f643
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-14 22:34:17 +0900

    修正

--- use_dayjs/second.ts
+++ use_dayjs/second.ts
@@ -5,10 +5,10 @@ dayjs.extend(utc);
 
 
 
-const roundSecond = (rowSecond: number, unitValue: number, das: string) => {
+const roundSecond = (rowSecond: number, unitValue: number) => {
   const remainder = rowSecond % unitValue;
 
-  console.log("e", dayjs(das).add(rowSecond + (unitValue - remainder), 's').format('YYYY-MM-DD HH:mm:ss'))
+  // console.log("e", dayjs("das").add(rowSecond + (unitValue - remainder), 's').format('YYYY-MM-DD HH:mm:ss'))
   // return dayjs(das).add(rowSecond + (unitValue - remainder), 's')
   return rowSecond + (unitValue - remainder);
 
@@ -19,8 +19,9 @@ const roundSecond = (rowSecond: number, unitValue: number, das: string) => {
 
 const cT = () => {
   const currentUtc = dayjs().utc();
-  const currentUtcString = currentUtc.format('YYYY-MM-DD HH:mm');
-  return currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm:') + roundSecond(currentUtc.second(), 5, currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm'));
+  const currentUtcString = currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm');
+  return dayjs(currentUtcString).add(roundSecond(currentUtc.second(), 5));
+  // return currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm:') + roundSecond(currentUtc.second(), 5, currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm'));
 }
 
 console.log(cT());
