24b52a155af9a80b2f6c2de97d978da23478d4df
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-14 22:36:11 +0900

    修正

--- use_dayjs/second.ts
+++ use_dayjs/second.ts
@@ -7,11 +7,7 @@ dayjs.extend(utc);
 
 const roundSecond = (rowSecond: number, unitValue: number) => {
   const remainder = rowSecond % unitValue;
-
-  // console.log("e", dayjs("das").add(rowSecond + (unitValue - remainder), 's').format('YYYY-MM-DD HH:mm:ss'))
-  // return dayjs(das).add(rowSecond + (unitValue - remainder), 's')
   return rowSecond + (unitValue - remainder);
-
 }
 
 
@@ -21,7 +17,6 @@ const cT = () => {
   const currentUtc = dayjs().utc();
   const currentUtcString = currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm');
   return dayjs(currentUtcString).add(roundSecond(currentUtc.second(), 5)).format('YYYY-MM-DD HH:mm:ss');
-  // return currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm:') + roundSecond(currentUtc.second(), 5, currentUtc.add(9, 'hour').format('YYYY-MM-DD HH:mm'));
 }
 
 console.log(cT());
