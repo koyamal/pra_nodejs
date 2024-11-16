92068230330deafd05c00c018cc6244b39e84438
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-16 21:43:21 +0900

    修正

--- use_dayjs/sp.js
+++ use_dayjs/sp.js
@@ -1,3 +1,5 @@
 "use strict";
 const arrayData = [1, 2, 3];
 console.log(...arrayData);
+const objData = { one: "one", two: "two", three: "three" };
+console.log(Object.assign({ four: "four" }, objData));
