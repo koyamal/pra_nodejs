35d891eb35a63b84c4e84f09f8d6413a47f216bf
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-17 20:17:26 +0900

    修正

--- use_dayjs/sp.js
+++ use_dayjs/sp.js
@@ -3,3 +3,8 @@ const arrayData = [1, 2, 3];
 console.log(...arrayData);
 const objData = { one: "one", two: "two", three: "three" };
 console.log(Object.assign({ four: "four" }, objData));
+const outputData = [
+    ...[1, 2, 3],
+    4
+];
+console.log(outputData);
--- use_dayjs/sp.ts
+++ use_dayjs/sp.ts
@@ -5,3 +5,11 @@ console.log(...arrayData);
 const objData = {one: "one", two: "two", three: "three"};
 
 console.log({four: "four", ...objData});
+
+const outputData = [
+  ...[1, 2, 3],
+  4,
+  ...(true? [5]: [6])
+];
+
+console.log(outputData);
