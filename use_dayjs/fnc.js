34b1a98c756ffe4130578eb93b423fee4f0386aa
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-23 23:04:17 +0900

    修正

--- use_dayjs/fnc.js
+++ use_dayjs/fnc.js
@@ -1,17 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.trueFanc = exports.isFanc = void 0;
+exports.trueFalseFunc = exports.trueFanc = exports.isFanc = void 0;
 const value = 1;
 const isFanc = () => {
     return value === 1;
 };
 exports.isFanc = isFanc;
-const trueFanc = () => {
-    return value !== 1;
-};
+const trueFanc = () => { value !== 1; };
 exports.trueFanc = trueFanc;
 const trueFalseFunc = () => {
     if (value)
         return 1;
     return 2;
 };
+exports.trueFalseFunc = trueFalseFunc;
