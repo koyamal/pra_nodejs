83bea90c2818925d40091d039edd7a7580177308
koyamal <74910401+koyamal@users.noreply.github.com>
2024-11-25 21:01:24 +0900

    修正

--- use_dayjs/fnc.ts
+++ use_dayjs/fnc.ts
@@ -14,6 +14,6 @@ const falseTrue = () => {
   return value % 2;
 }
 
-const trueFlagFanc = () => value % 2 === 1? 0: 1;
+const trueFlagFanc = () => value % 2 === 1 ? 0 : 1;
 
 export { isFanc, trueFanc, trueFalseFunc, falseTrue, trueFlagFanc }
