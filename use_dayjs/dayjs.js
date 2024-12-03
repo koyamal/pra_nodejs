79c1288d4280a524f27ac419d469370004c4a0f6
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-03 23:11:37 +0900

    修正

--- use_dayjs/dayjs.js
+++ use_dayjs/dayjs.js
@@ -3,12 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.customConsole = void 0;
 const dayjs_1 = __importDefault(require("dayjs"));
 const utc_1 = __importDefault(require("dayjs/plugin/utc"));
 const timezone_1 = __importDefault(require("dayjs/plugin/timezone"));
 const customConsole = (val) => {
     console.log(val);
 };
+exports.customConsole = customConsole;
 dayjs_1.default.extend(timezone_1.default);
 dayjs_1.default.extend(utc_1.default);
 const nowNoTz = (0, dayjs_1.default)();
