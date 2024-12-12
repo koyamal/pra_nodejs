46d124b1857c5c2c6c988bbb789e0a02ce76b783
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-12 20:47:27 +0900

    修正

--- use_object/object.ts
+++ use_object/object.ts
@@ -1,5 +1,5 @@
 const constParams = {};
 Object.assign(constParams, {a: 'valueA', b: 'valueB'});
-Object.assign(constParams, {c: 'valueC', d: {e: 'valueE'}});
+Object.assign(constParams, {c: 'valueC', d: {e: 'valueE', f: 'valueF'}});
 
 console.log(constParams);
