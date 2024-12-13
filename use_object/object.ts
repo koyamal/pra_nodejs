30ae0263ffe7e9039e63bba0e7a7b01ba879b741
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-13 18:26:22 +0900

    修正

--- use_object/object.ts
+++ use_object/object.ts
@@ -3,3 +3,10 @@ Object.assign(constParams, {a: 'valueA', b: 'valueB'});
 Object.assign(constParams, {c: 'valueC', d: {e: 'valueE', f: 'valueF'}});
 
 console.log(constParams);
+
+const animal = {
+  isPlant: false,
+  printHello: function() {
+
+  }
+}
