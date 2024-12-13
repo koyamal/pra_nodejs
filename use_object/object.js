1fc90de67cb3b43aa281744fea3ff6072c073dbd
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-13 18:28:38 +0900

    修正

--- use_object/object.js
+++ use_object/object.js
@@ -3,3 +3,10 @@ const constParams = {};
 Object.assign(constParams, { a: 'valueA', b: 'valueB' });
 Object.assign(constParams, { c: 'valueC', d: { e: 'valueE', f: 'valueF' } });
 console.log(constParams);
+const animal = {
+    isPlant: false,
+    name: '',
+    printHello: function () {
+        console.log(`Hello, I'm ${this.name}.` + (this.isPlant ? 'Plant.' : 'Not Plant'));
+    }
+};
