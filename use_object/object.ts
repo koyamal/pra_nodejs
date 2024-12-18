01d9dc3e4c88d4d49a275aff96c6ac86a35ed957
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-18 21:19:39 +0900

    修正

--- use_object/object.ts
+++ use_object/object.ts
@@ -47,3 +47,11 @@ console.log(tree.price);
 
 tree.taxedPrice = 600 * 1.1; // エラーになる
 console.log(tree.taxedPrice);
+
+const originValue = {
+  maxInstances: 5,
+  minInstances: 2,
+  value: 12
+};
+
+const {maxInstances, minInstances} = originValue;
