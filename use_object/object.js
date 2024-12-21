74c81e92ef033395a31070b0460a4e17b419653e
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-21 20:55:13 +0900

    修正

--- use_object/object.js
+++ use_object/object.js
@@ -48,4 +48,4 @@ console.log(maxInstances);
 const { value } = originValue;
 console.log(value);
 const dateString = '2024-11-21 10:00';
-console.log(dateString.slice(0, 10).replace('-', '.'));
+console.log(dateString.slice(0, 10).replace(/-/g, '.'));
