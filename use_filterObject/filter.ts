50de372326901930dbcd68723431704ba88fc698
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-23 18:57:24 +0900

    修正

--- use_filterObject/filter.ts
+++ use_filterObject/filter.ts
@@ -1,8 +1,8 @@
 const excludeProps = ['A', 'C', 'E', 'G', 'H'];
 
-const c = 'A'.charCodeAt(0);
-const alphabets = Array.apply(null, new Array(26)).map((v, i) => {
-  return String.fromCharCode(c + i);
+const startPoint = 'A'.charCodeAt(0);
+const alphabets = Array.apply(null, new Array(26)).map((_, i) => {
+  return String.fromCharCode(startPoint + i);
 });
 
 console.log(alphabets);
