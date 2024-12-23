bdcaeafc0414942f064bcdab5eeb2b84d7d7a923
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-23 18:57:46 +0900

    修正

--- use_filterObject/filter.js
+++ use_filterObject/filter.js
@@ -1,7 +1,7 @@
 "use strict";
 const excludeProps = ['A', 'C', 'E', 'G', 'H'];
-const c = 'A'.charCodeAt(0);
-const alphabets = Array.apply(null, new Array(26)).map((v, i) => {
-    return String.fromCharCode(c + i);
+const startPoint = 'A'.charCodeAt(0);
+const alphabets = Array.apply(null, new Array(26)).map((_, i) => {
+    return String.fromCharCode(startPoint + i);
 });
 console.log(alphabets);
