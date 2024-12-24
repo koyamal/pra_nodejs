08f06b7946914851579c0c67381013c2e9704955
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-24 22:25:35 +0900

    修正

--- use_filterObject/filter.ts
+++ use_filterObject/filter.ts
@@ -3,7 +3,7 @@ const alphabets = Array.apply(null, new Array(26)).map((_, i) => {
   return String.fromCharCode(startPoint + i);
 });
 
-const excludeProps = ['A', 'C', 'E', 'G', 'H', 'Z'];
+const excludeProps = ['A', 'C', 'E', 'G', 'H', 'G', 'Z'];
 
 const filteredAlphabets = alphabets.filter(alphabet => !excludeProps.includes(alphabet));
 
