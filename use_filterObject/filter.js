1c6f4ac727ab5dbd6c6a549821446e2d211b54cc
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-29 22:43:05 +0900

    修正

--- use_filterObject/filter.js
+++ use_filterObject/filter.js
@@ -15,4 +15,4 @@ filteredAlphabets.forEach((data) => {
 });
 console.log(queryText.slice(0, queryText.length - 5) + ';');
 console.log(mapedAlphabets);
-let textUpdate = 'update XXXXX set dataA = "12345" where dataB = "12345"';
+const textUpdate = 'update XXXXX set dataA = "12345" where dataB = "12345"';
