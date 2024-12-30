bcfbf46996afd3fd251440b03869a1a8aa0ebd3a
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-30 23:40:25 +0900

    修正

--- use_filterObject/filter.js
+++ use_filterObject/filter.js
@@ -1,8 +1,6 @@
 "use strict";
 const startPoint = 'A'.charCodeAt(0);
-const alphabets = Array.apply(null, new Array(26)).map((_, i) => {
-    return String.fromCharCode(startPoint + i);
-});
+const alphabets = Array.apply(null, new Array(26)).map((_, i) => String.fromCharCode(startPoint + i));
 const excludeProps = ['A', 'C', 'E', 'G', 'H', 'G', 'Z'].filter(data => data != 'G');
 const filteredAlphabets = alphabets.filter(alphabet => !excludeProps.includes(alphabet));
 console.log(filteredAlphabets);
