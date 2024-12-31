9911f78cd65e9edc6da22d4c51c8edc85188de59
koyamal <74910401+koyamal@users.noreply.github.com>
2024-12-31 21:08:01 +0900

    修正

--- use_filterObject/filter.ts
+++ use_filterObject/filter.ts
@@ -9,9 +9,9 @@ console.log(filteredAlphabets);
 
 const mapedAlphabets = filteredAlphabets.map(data => data + ',');
 
-let queryText = 'select * from XXXXX where ';
+let outputText = 'select * from XXXXX where ';
 filteredAlphabets.forEach((data) => {
-  queryText += data + ' is not null and ';
+  outputText += data + ' is not null and ';
 });
 
 console.log(queryText.slice(0, queryText.length - 5) + ';');
