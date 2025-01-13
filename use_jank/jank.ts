5cc832b9da1c4129db43c7519acc998ad3e4cb68
koyamal <74910401+koyamal@users.noreply.github.com>
2025-01-13 16:53:09 +0900

    修正

--- use_jank/jank.ts
+++ use_jank/jank.ts
@@ -3,6 +3,6 @@ import * as readline from "node:readline";
 
 const rl = readline.createInterface(stdin, stdout);
 
-rl.question("hello", answer => {
+rl.question("じゃんけん(1.ぐー、2.ちょき、3.ぱー)：", answer => {
   console.log(answer);
 });
