d07d59d9c036b33e98e179555e3f7024f1e4dc2a
koyamal <74910401+koyamal@users.noreply.github.com>
2025-01-21 20:24:59 +0900

    修正

--- use_jank/jank.ts
+++ use_jank/jank.ts
@@ -28,9 +28,8 @@ rl.question("じゃんけん(1.ぐー、2.ちょき、3.ぱー)：", answer => {
   } else {
     console.log('エラー');
   }
-});
-
-rl.question('結果を見ますか？', (input) => {
-  console.log(`入力内容: ${input}`);
-  rl.close();
+  rl.question('結果を見ますか？', (input) => {
+    console.log(`入力内容: ${input}`);
+    rl.close();
+  });
 });
