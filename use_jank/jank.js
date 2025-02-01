01698103817ff27e2451b19d73e054db2e763756
koyamal <74910401+koyamal@users.noreply.github.com>
2025-02-01 23:13:40 +0900

    修正

--- use_jank/jank.js
+++ use_jank/jank.js
@@ -25,6 +25,7 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const node_process_1 = require("node:process");
 const readline = __importStar(require("node:readline"));
+const rl = readline.createInterface(node_process_1.stdin, node_process_1.stdout);
 const jadgeJank = (userHand, cpuHand) => {
     switch (userHand) {
         case 1:
@@ -64,7 +65,6 @@ const jadgeJank = (userHand, cpuHand) => {
             return 'エラー';
     }
 };
-const rl = readline.createInterface(node_process_1.stdin, node_process_1.stdout);
 const handAndText = {
     1: 'ぐー',
     2: 'ちょき',
