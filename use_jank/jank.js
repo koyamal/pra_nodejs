1dcb8ff8e27cbe534458fea17343e0b186f37609
koyamal <74910401+koyamal@users.noreply.github.com>
2025-02-03 21:27:58 +0900

    修正

--- use_jank/jank.js
+++ use_jank/jank.js
@@ -108,7 +108,7 @@ const playJanken = () => {
                 console.log(`結果：${jadgeJank(userHand, cpuHand)}`);
             }
             rl.question('もう一度遊びますか？(y/n)：', input => {
-                if (input === 'y') {
+                if (input.toLowerCase() === 'y') {
                     playJanken();
                 }
                 else {
