9f5f94f4465d4572c015b781d930506a7c853e57
koyamal <74910401+koyamal@users.noreply.github.com>
2025-02-07 18:32:06 +0900

    修正

--- use_compStr/comp.js
+++ use_compStr/comp.js
@@ -2,9 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const targetDate = '2025-01-02 09:00';
 const compareDate = [
+    '2024-01-02 09:00:00',
     '2025-01-02 08:59:59',
     '2025-01-02 09:00:00',
     '2025-01-02 09:00:01',
+    '2025-01-03 08:59:59',
+    '2025-01-03 09:00:00',
 ];
 compareDate.forEach((date) => {
     if (targetDate >= date) {
