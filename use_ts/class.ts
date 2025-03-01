1e26304c644cb92b383fca57ead394b4c405a4f6
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-01 16:39:38 +0900

    修正

--- use_ts/class.ts
+++ use_ts/class.ts
@@ -23,4 +23,5 @@ const psession = new Psession({id: '12344', bId: 'dboeiradkm', cId: null, type: 
 psession.outputLog();
 
 const psession2 = new Psession({id: '54323456', bId: 'sdfawerjfsdlkjsad', cId: 'dfjsakfjfsdk', type: 'B'});
+const psession3 = new Psession({id: '54323456', bId: 'sdfawerjfsdlkjsad', cId: 'dfjsakfjfsdk', type: 'A'});
 psession2.outputLog();
