0a5b7a6fbe42a2f51eb9c731c3eae4498e9460f2
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-27 22:04:28 +0900

    作成

---
+++ use_some/some.js
@@ -0,0 +1,9 @@
+"use strict";
+const array1 = [
+    { id: '1', data: 'hello' },
+    { id: '2', data: 'bye' },
+    { id: '3', data: 'yes' },
+];
+const array2 = [
+    { id: '2', data: 'bye' },
+];
