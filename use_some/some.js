9952c24eada8b81a4e53884aaf71ef42cdb98486
koyamal <74910401+koyamal@users.noreply.github.com>
2025-03-29 23:19:18 +0900

    修正

--- use_some/some.js
+++ use_some/some.js
@@ -1,11 +1,11 @@
 "use strict";
 const array1 = [
     { id: '1', data: 'hello' },
-    // {id: '2', data: 'bye'},
+    { id: '2', data: 'bye' },
     { id: '3', data: 'yes' },
 ];
 const array2 = [
-    // {id: '0', data: 'zero'},
+    { id: '0', data: 'zero' },
     { id: '2', data: 'bye' },
 ];
 const array3 = array1.some(d => array2.some(da => da.id === d.id));
