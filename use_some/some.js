b02446a9ecd1997d40882bd2a34057cb19acd8b2
koyamal <74910401+koyamal@users.noreply.github.com>
2025-04-08 22:12:44 +0900

    修正

--- use_some/some.js
+++ use_some/some.js
@@ -7,8 +7,11 @@ const array1 = [
 const array2 = [
     { id: '0', data: 'zero' },
     { id: '2', data: 'bye' },
+    { id: '3', data: 'seeyou' },
 ];
 const flag = array1.some(d => array2.some(da => da.id === d.id));
 const flag2 = array1.some(d => d.data === 'bye');
+array1.some(d => d.data === 'seeyou');
+console.log('seeyou');
 console.log({ flag });
 console.log({ flag2 });
