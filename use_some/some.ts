112b91fb7dbb2e343ba5c10507e8d331735036a3
koyamal <74910401+koyamal@users.noreply.github.com>
2025-04-25 21:37:30 +0900

    修正

--- use_some/some.ts
+++ use_some/some.ts
@@ -17,6 +17,7 @@ const flag = array1.some(d => array2.some(da => da.id === d.id));
 
 const flag2 = array1.some(d => d.data === 'bye');
 flag2 && console.log('true');
+flag && console.log('true');
 
 array1.some(d => d.data === 'seeyou');
 console.log('seeyou');
