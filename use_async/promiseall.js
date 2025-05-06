42ae6764b88e0cf3522941b5b7dc103825f3f651
koyamal <74910401+koyamal@users.noreply.github.com>
2025-05-06 22:35:52 +0900

    修正

--- use_async/promiseall.js
+++ use_async/promiseall.js
@@ -50,7 +50,7 @@ const doPromiseAll = () => __awaiter(void 0, void 0, void 0, function* () {
 const doPromiseAllResolve = (num) => __awaiter(void 0, void 0, void 0, function* () {
     console.log(`${num} is start`);
     const res = yield myPromise(num);
-    console.log(res);
+    console.log(`${num} is done: res=${res}`);
 });
 const doPromiseAllSettled = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
