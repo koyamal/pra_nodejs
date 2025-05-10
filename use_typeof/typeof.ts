da7bbfcca2acc6e466643093623c90415b88f793
koyamal <74910401+koyamal@users.noreply.github.com>
2025-05-10 10:43:23 +0900

    修正

---
+++ use_typeof/typeof.ts
@@ -0,0 +1,13 @@
+import { type } from "os";
+
+const metaTitle = '';
+
+const checkTypeOf = (metaTitle: any) => {
+  if(typeof metaTitle === 'string') {
+    console.log('metaTitleのtypeはstringです。');
+    return 'string';
+  } else {
+    console.log('metaTitleのtypeはstringではありません。');
+    return 'not string';
+  }
+} 
