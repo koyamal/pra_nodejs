e871b95c3f1ebd7d397821ff4fcaee50a972f75b
koyamal <74910401+koyamal@users.noreply.github.com>
2025-05-10 10:43:30 +0900

    修正

---
+++ use_typeof/typeof.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const metaTitle = '';
+const checkTypeOf = (metaTitle) => {
+    if (typeof metaTitle === 'string') {
+        console.log('metaTitleのtypeはstringです。');
+        return 'string';
+    }
+    else {
+        console.log('metaTitleのtypeはstringではありません。');
+        return 'not string';
+    }
+};
