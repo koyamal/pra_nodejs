46bd2ff70c82812e76f6a136fe036449f5133231
koyamal <74910401+koyamal@users.noreply.github.com>
2025-05-12 23:16:22 +0900

    修正

--- use_typeof/typeof.ts
+++ use_typeof/typeof.ts
@@ -1,10 +1,8 @@
 const checkTypeOf = (val: any) => {
   console.log(`${val}のtypeは${typeof val}です。`);
   if(typeof val === 'string') {
-    console.log(`${val}のtypeはstringです。`);
     return true;
   } else {
-    console.log(`${val}のtypeは${typeof val}です。`);
     return false;
   }
 }
