d435e23fe450c9a557f1c018641282f960fb2af0
koyamal <74910401+koyamal@users.noreply.github.com>
2025-05-17 23:30:31 +0900

    修正

--- use_typeof/typeof.js
+++ use_typeof/typeof.js
@@ -3,7 +3,7 @@ const checkTypeOf = (val) => {
     console.log(`${val}のtypeは${typeof val}です。`);
     return typeof val === 'string';
 };
-checkTypeOf('');
+checkTypeOf('') && console.log('hello');
 checkTypeOf(undefined);
 checkTypeOf(2);
 checkTypeOf(null);
