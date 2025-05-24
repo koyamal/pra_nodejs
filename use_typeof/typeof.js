ac6c7c392b76d37db2d3cdaddd0c1ec827359c8c
koyamal <74910401+koyamal@users.noreply.github.com>
2025-05-24 21:32:24 +0900

    修正

--- use_typeof/typeof.js
+++ use_typeof/typeof.js
@@ -3,12 +3,12 @@ const checkTypeOf = (val) => {
     console.log(`${val}のtypeは${typeof val}です。`);
     return typeof val === 'string';
 };
-const checkTypeOfIndex = (val) => {
+const logKeys = (val) => {
     Object.keys(val).map((key) => {
         console.log(key);
     });
 };
-checkTypeOfIndex({ a: 1, b: 2 });
+logKeys({ a: 1, b: 2 });
 checkTypeOf('');
 checkTypeOf('hello');
 checkTypeOf(undefined);
