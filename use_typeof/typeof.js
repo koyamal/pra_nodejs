20f8bcca6f81d1173219822af4d4e2a84b703713
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-05 21:51:31 +0900

    修正

--- use_typeof/typeof.js
+++ use_typeof/typeof.js
@@ -32,7 +32,7 @@ const logObject = (val) => {
         Object.values(val).forEach(value => {
             console.log(value);
         });
-        return;
+        return null;
     }
     return val;
 };
