cf3ecdbeaec8140d7a55fc0b798fdd7bab902207
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-12 18:12:50 +0900

    修正

--- use_some/some.js
+++ use_some/some.js
@@ -21,7 +21,7 @@ console.log('seeyou');
 console.log({ flag });
 console.log({ flag2 });
 const skipUrls = ['/hello/world', '/good/value'];
-const requestUrl = '/hello/world/good';
+const requestUrl = '/hello/world';
 if (skipUrls.some(url => url.startsWith(requestUrl))) {
     console.log('skip');
 }
