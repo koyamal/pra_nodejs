75255b923de630449d0af056194774ebb7455185
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-13 21:17:59 +0900

    修正

--- use_floor/floor.ts
+++ use_floor/floor.ts
@@ -2,3 +2,8 @@ console.log('=======3.92=======');
 for(let i = 0; i < 16; i++) {
   console.log(Math.floor(Math.random() * 100 / 3.92));
 }
+
+console.log('=======0.392=======');
+for(let i = 0; i < 16; i++) {
+  console.log(Math.floor(Math.random() * 100 / 0.392));
+}
