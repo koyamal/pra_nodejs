1e111173512d913ad9c5ccd993c4e575f1726f23
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-14 22:39:14 +0900

    修正

--- use_floor/floor.js
+++ use_floor/floor.js
@@ -6,7 +6,7 @@ for (let i = 0; i < 16; i++) {
 }
 console.log('=======0.392=======');
 const arraybox = [];
-for (let i = 0; i < 16; i++) {
+for (let i = 0; i < 1000; i++) {
     arraybox.push(Math.floor(Math.random() * 100 / 0.392));
 }
 console.log(Math.max(...arraybox));
