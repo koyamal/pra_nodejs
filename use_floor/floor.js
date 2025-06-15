0fd67597a754ed793077756d794b9ea070ece836
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-15 22:44:22 +0900

    修正

--- use_floor/floor.js
+++ use_floor/floor.js
@@ -1,9 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 console.log('=======3.92=======');
-for (let i = 0; i < 16; i++) {
-    console.log(Math.floor(Math.random() * 100 / 3.92));
+const arraybox392 = [];
+for (let i = 0; i < 1000; i++) {
+    arraybox392.push(Math.floor(Math.random() * 100 / 3.92));
 }
+console.log('min:', Math.min(...arraybox392));
+console.log('max:', Math.max(...arraybox392));
 console.log('=======0.392=======');
 const arraybox0392 = [];
 for (let i = 0; i < 1000; i++) {
