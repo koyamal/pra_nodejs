e7de049a8757c966bc19d5a65fc5afcfd8951233
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-17 20:43:26 +0900

    修正

--- use_floor/floor.ts
+++ use_floor/floor.ts
@@ -23,4 +23,5 @@ for(let i = 0; i < 10000; i++) {
 
 console.log('min:', Math.min(...arraybox00392));
 console.log('max:', Math.max(...arraybox00392));
+console.log('max:', Math.fround(Math.max(...arraybox00392)));
 export {};
