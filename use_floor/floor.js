93f552a9b063d08d7030222abfa6fecbc1c8c24d
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-18 20:23:10 +0900

    修正

--- use_floor/floor.js
+++ use_floor/floor.js
@@ -22,3 +22,4 @@ for (let i = 0; i < 10000; i++) {
 console.log('min:', Math.min(...arraybox00392));
 console.log('max:', Math.max(...arraybox00392));
 console.log('max:', Math.fround(Math.max(...arraybox00392)));
+console.log('min:', Math.fround(Math.min(...arraybox00392)));
