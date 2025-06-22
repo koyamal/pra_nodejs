6dc58fa26f1dc410562eb649a7b5192722a2cdd2
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-22 17:54:48 +0900

    修正

--- use_floor/floor.ts
+++ use_floor/floor.ts
@@ -1,6 +1,6 @@
 console.log('=======3.92=======');
 const arraybox392 = [];
-for(let i = 0; i < 1000; i++) {
+for(let i = 0; i < 10000; i++) {
   arraybox392.push(Math.floor(Math.random() * 100 / 3.92));
 }
 console.log('min:', Math.min(...arraybox392));
@@ -8,7 +8,7 @@ console.log('max:', Math.max(...arraybox392));
 
 console.log('=======0.392=======');
 const arraybox0392 = [];
-for(let i = 0; i < 1000; i++) {
+for(let i = 0; i < 10000; i++) {
   arraybox0392.push(Math.floor(Math.random() * 100 / 0.392));
 }
 
