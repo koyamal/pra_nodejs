2c4e5a2475f78c86a34b0bc01ca94bcefd5f07fd
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-25 22:29:29 +0900

    修正

--- use_floor/floor.js
+++ use_floor/floor.js
@@ -39,3 +39,6 @@ console.log(`${indexColor}: ${randomColor}`);
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+for (let i = 0; i < 10; i++) {
+    console.log(getRandomInt(2, 10));
+}
