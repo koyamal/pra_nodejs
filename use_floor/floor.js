d2928f053e9bb7f5f7a6ddd24c430c361534b1d3
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-25 22:28:36 +0900

    修正

--- use_floor/floor.js
+++ use_floor/floor.js
@@ -36,3 +36,6 @@ const colors = ['red', 'blue', 'green'];
 const indexColor = Math.floor(Math.random() * colors.length);
 const randomColor = colors[indexColor];
 console.log(`${indexColor}: ${randomColor}`);
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
