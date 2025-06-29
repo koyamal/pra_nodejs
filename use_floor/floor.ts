e04d48df4fc3bcb5c17b6f834666b8d25f6b9eb6
koyamal <74910401+koyamal@users.noreply.github.com>
2025-06-29 17:20:44 +0900

    修正

--- use_floor/floor.ts
+++ use_floor/floor.ts
@@ -57,5 +57,6 @@ function getRandomRGBColor() {
 }
 
 console.log(getRandomRGBColor());
-console.log(`\x1b[38;2;${10};${10};${10}}mこの文字はRGB色付きです\x1b[0m`);
+const r = 255, g = 100, b = 50;
+console.log(`\x1b[38;2;${r};${g};${b}mこの文字はRGB色付きです\x1b[0m`);
 export {};
