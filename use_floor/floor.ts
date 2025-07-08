fe4c4e6b4750f031761379aac1a0907db8b94dda
koyamal <74910401+koyamal@users.noreply.github.com>
2025-07-08 22:48:25 +0900

    修正

--- use_floor/floor.ts
+++ use_floor/floor.ts
@@ -47,6 +47,8 @@ function getRandomInt(min: number, max: number) {
 }
 
 console.log(getRandomInt(10, 13));
+console.log(getRandomInt(100, 13));
+console.log(getRandomInt(10, 139));
 console.log(getRandomInt(100, 130));
 
 function getRandomFloat(min: number, max: number) {
