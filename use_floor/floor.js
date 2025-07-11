ade5f6c024fe624f1c251336e3c0db571698c317
koyamal <74910401+koyamal@users.noreply.github.com>
2025-07-11 20:36:08 +0900

    修正

--- use_floor/floor.js
+++ use_floor/floor.js
@@ -44,8 +44,6 @@ function getRandomInMax(min, max) {
     return tmpValue * max;
 }
 console.log(getRandomInt(10, 13));
-console.log(getRandomInt(100, 13));
-console.log(getRandomInt(10, 139));
 console.log(getRandomInMax(100, 130));
 function getRandomFloat(min, max) {
     return Math.random() * (max - min) + min;
