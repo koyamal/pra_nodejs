8a146f9c8dd6f9948921520307ec6868cd16a3c4
koyamal <74910401+koyamal@users.noreply.github.com>
2025-07-12 21:01:14 +0900

    修正

--- use_floor/floor.ts
+++ use_floor/floor.ts
@@ -1,6 +1,8 @@
+const randomCount = 1000;
+
 console.log('=======39.2=======');
 const arraybox3920 = [];
-for(let i = 0; i < 1000; i++) {
+for(let i = 0; i < randomCount; i++) {
   arraybox3920.push(Math.floor(Math.random() * 100 / 39.2));
 }
 console.log('min:', Math.min(...arraybox3920));
