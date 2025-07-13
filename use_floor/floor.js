f30b1fe5ff9be2add46c8dc30eca50b41411ae6e
koyamal <74910401+koyamal@users.noreply.github.com>
2025-07-13 17:55:44 +0900

    修正

--- use_floor/floor.js
+++ use_floor/floor.js
@@ -17,10 +17,7 @@ const arraybox392 = createRandomArray(3.92);
 console.log('min:', Math.min(...arraybox392));
 console.log('max:', Math.max(...arraybox392));
 console.log('=======0.392=======');
-const arraybox0392 = [];
-for (let i = 0; i < randomCount; i++) {
-    arraybox0392.push(Math.floor(Math.random() * 100 / 0.392));
-}
+const arraybox0392 = createRandomArray(0.392);
 console.log('min:', Math.min(...arraybox0392));
 console.log('max:', Math.max(...arraybox0392));
 console.log('=======0.0392=======');
