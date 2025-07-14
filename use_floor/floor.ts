94214cce283bd1301fe52958b2b17a53971964a0
koyamal <74910401+koyamal@users.noreply.github.com>
2025-07-14 20:34:28 +0900

    修正

--- use_floor/floor.ts
+++ use_floor/floor.ts
@@ -16,11 +16,7 @@ const createRandomArrayWithConsole = (divValue: number) => {
 }
 createRandomArrayWithConsole(39.2);
 createRandomArrayWithConsole(3.92);
-
-console.log('=======0.392=======');
-const arraybox0392 = createRandomArray(0.392);
-console.log('min:', Math.min(...arraybox0392));
-console.log('max:', Math.max(...arraybox0392));
+createRandomArrayWithConsole(0.392);
 
 console.log('=======0.0392=======');
 const arraybox00392 = createRandomArray(0.0392);
