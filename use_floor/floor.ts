7adcdb338d1218e877ee1433c92351fb974c1a10
koyamal <74910401+koyamal@users.noreply.github.com>
2025-07-15 20:33:42 +0900

    修正

--- use_floor/floor.ts
+++ use_floor/floor.ts
@@ -16,6 +16,7 @@ createRandomArrayWithConsole(39.2, 1000);
 createRandomArrayWithConsole(3.92, 1000);
 createRandomArrayWithConsole(0.392, 10000);
 createRandomArrayWithConsole(0.0392, 10000);
+createRandomArrayWithConsole(0.00392, 100000);
 
 const getRandomInt = (min: number, max: number) => {
   return Math.floor(Math.random() * (max - min + 1)) + min;
